65ca74ec2fa9e8be20a590636cb0ff7f723eb566
yaskevich <alyaxey.yaskevich@gmail.com>
2021-02-16 23:43:37 +0300

    add main UI markup

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,84 @@
-<template>  
-  <HelloWorld msg="Проект" />
+<template>
+
+<el-container style="margin-top:-4rem; border: 1px solid #eee">
+  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+    <el-menu :default-openeds="['1', '3']">
+      <el-submenu index="1">
+        <template #title><i class="el-icon-message"></i>Пользователи</template>
+        <el-menu-item-group>
+          <template #title>Group 1</template>
+          <el-menu-item index="1-1">Option 1</el-menu-item>
+          <el-menu-item index="1-2">Option 2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group 2">
+          <el-menu-item index="1-3">Option 3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template #title>Option4</template>
+          <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-submenu index="2">
+        <template #title><i class="el-icon-menu"></i>Navigator Two</template>
+        <el-menu-item-group>
+          <template #title>Group 1</template>
+          <el-menu-item index="2-1">Option 1</el-menu-item>
+          <el-menu-item index="2-2">Option 2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group 2">
+          <el-menu-item index="2-3">Option 3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="2-4">
+          <template #title>Option 4</template>
+          <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-submenu index="3">
+        <template #title><i class="el-icon-setting"></i>Navigator Three</template>
+        <el-menu-item-group>
+          <template #title>Group 1</template>
+          <el-menu-item index="3-1">Option 1</el-menu-item>
+          <el-menu-item index="3-2">Option 2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group 2">
+          <el-menu-item index="3-3">Option 3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="3-4">
+          <template #title>Option 4</template>
+          <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+    </el-menu>
+  </el-aside>
+
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <template #dropdown>
+          <el-dropdown-menu>
+              <el-dropdown-item>View</el-dropdown-item>
+              <el-dropdown-item>Add</el-dropdown-item>
+              <el-dropdown-item>Delete</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+      <span>Tom</span>
+    </el-header>
+
+    <el-main>
+      <!-- <div class="common-layout">
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main> -->
+          Main
+          <HelloWorld msg="Проект" />
+        <!-- </el-main>
+      </el-container>
+    </div> -->
+    </el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script lang="ts">
@@ -7,7 +86,7 @@ import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: 'App',  
+  name: 'App',
   components: {
     HelloWorld
   }
@@ -23,4 +102,13 @@ export default defineComponent({
   color: #2c3e50;
   margin-top: 60px;
 }
+.el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+  }
 </style>

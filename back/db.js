e6836d2010d3547b9b8c8a06ed0492da96e3bbb8
yaskevich <alyaxey.yaskevich@gmail.com>
2021-02-21 23:57:07 +0300

    persons table [SQL]

--- back/db.js
+++ back/db.js
@@ -4,6 +4,17 @@ import pg from 'pg';
 const { Pool } = pg;
 const pool = new Pool();
 
+`CREATE TABLE persons (
+	id SERIAL PRIMARY KEY,
+	firstName text not null,
+	lastName text,
+	patroName text,
+	sex integer,
+	wikidata integer,
+	features jsonb
+)`;
+
+
 export default {
 	async getPersons(){
 		const res = await pool.query('select * from persons');

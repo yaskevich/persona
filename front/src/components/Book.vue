1020c14c5e0ba93df35fea5f4856842799806ac7
yaskevich <alyaxey.yaskevich@gmail.com>
2021-02-28 23:56:23 +0300

    render book component

--- front/src/components/Book.vue
+++ front/src/components/Book.vue
@@ -5,11 +5,67 @@
   </el-form-item>
 
   <el-form-item prop="genre">
-    <el-input placeholder="Тип" v-model="form.genre" class="text-input"></el-input>
+    <el-input placeholder="Год" v-model="form.year" class="text-input"></el-input>
   </el-form-item>
 
-  <el-button type="primary" @click="resetForm">Очистить</el-button>
-  <el-button type="primary" @click="confirm">Добавить</el-button>
+<el-form-item>
+  <el-select
+      v-model="selectedWorks"
+      multiple
+      filterable
+      remote
+      reserve-keyword
+      placeholder="Произведение"
+      :remote-method="getWorks"
+      :loading="loading">
+      <el-option
+        v-for="item in works"
+        :key="item.id"
+        :label="item.title"
+        :value="item.title">
+      </el-option>
+    </el-select>
+  </el-form-item>
+  <el-form-item>
+  <el-select
+      v-model="selectedEditors"
+      multiple
+      filterable
+      remote
+      reserve-keyword
+      placeholder="Редакторы"
+      :remote-method="getEditors"
+      :loading="loading">
+      <el-option
+        v-for="item in editors"
+        :key="item.id"
+        :label="item.value"
+        :value="item.value">
+      </el-option>
+    </el-select>
+  </el-form-item>
+  <el-form-item>
+  <el-select
+      v-model="selectedEditors"
+      multiple
+      filterable
+      remote
+      reserve-keyword
+      placeholder="Авторы"
+      :remote-method="getEditors"
+      :loading="loading">
+      <el-option
+        v-for="item in editors"
+        :key="item.id"
+        :label="item.value"
+        :value="item.value">
+      </el-option>
+    </el-select>
+  </el-form-item>
+
+    <el-button type="primary" @click="resetForm">Очистить</el-button>
+    <el-button type="primary" @click="confirm">Добавить</el-button>
+
 </el-form>
 </template>
 
@@ -22,15 +78,47 @@ export default defineComponent({
   setup() {
     const formRef = ref<InstanceType<typeof ElForm>>();
 
-    const works = reactive([]);
+    const works = ref([]);
+    let persons = [];
+    const loading = ref(false);
+    const selectedWorks = ref([]);
+    const selectedEditors = ref([]);
+    const editors = ref([]);
+
 
     onBeforeMount(async() => {
-      axios.get('/api/get/works').then((response) => {
+      axios.get('/api/get/persons')
+      .then((response) => {
         console.log(response.data);
-        works.push(...response.data);
+        persons = response.data.map (x => ({...x, value: x.firstname + ' ' + x.lastname}) );
+        console.log(persons);
+        editors.value  = persons;
       })
-    })
+      axios.get('/api/get/works').then((response) => {
+        // console.log(response.data);
+        works.value  = response.data;
+      })
+    });
+    const getWorks = (query) => {
+      console.log(query);
+      const re  = new RegExp(query, "i");
+      loading.value = true;
+      axios.get('/api/get/works').then((response) => {
+        // console.log(response.data);
+        // works.value  = response.data;
+        works.value = response.data.filter(x => re.test(x.title));
+      })
+      loading.value = false;
+    };
 
+    const getEditors = (query) => {
+      console.log(query);
+      const re  = new RegExp(query, "i");
+      loading.value = true;
+      editors.value = persons.filter(x => re.test(x.value));
+
+      loading.value = false;
+    };
 
     const resetForm = () => {
       formRef.value?.resetFields();
@@ -42,15 +130,15 @@ export default defineComponent({
           console.log("here");
           // return false;
 
-          axios.post('/api/work/add', form)
-            .then(function (response) {
-              console.log(response);
-              works.unshift({...form});
-              formRef.value?.resetFields();
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
+          // axios.post('/api/work/add', form)
+          //   .then(function (response) {
+          //     console.log(response);
+          //     works.unshift({...form});
+          //     formRef.value?.resetFields();
+          //   })
+          //   .catch(function (error) {
+          //     console.log(error);
+          //   });
 
         }  else{
           return false;
@@ -70,6 +158,12 @@ export default defineComponent({
     };
 
     return {
+      editors,
+      selectedEditors,
+      getEditors,
+      selectedWorks,
+      getWorks,
+      loading,
       resetForm,
       confirm,
       formRef,

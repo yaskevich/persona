211cfd8a1cdff2060f97ea54b8b0850b865c3138
yaskevich <alyaxey.yaskevich@gmail.com>
2021-03-20 11:03:25 +0300

    render event component [front]

--- front/src/components/Event.vue
+++ front/src/components/Event.vue
@@ -1,14 +1,95 @@
 <template>
-    <el-form label-width="120px" v-model="user" :inline="true">
-      <el-form-item prop="date">
-        <el-input placeholder="Дата" v-model="user.firstname" class="text-input" prop="firstName"></el-input>
+    <el-form label-width="120px" v-model="event">
+      <el-form-item prop="date" label="Дата">
+        <el-input placeholder="10.05.1928" v-model="event.firstname" class="text-input" prop="firstName"></el-input>
       </el-form-item>
-      <el-form-item prop="tags">
-        Тег
+      <el-form-item prop="tags" label="Теги">
+        <el-input placeholder="переписка (БП_отправитель)" v-model="event.firstname"></el-input>
       </el-form-item>
 
-    <el-form-item prop="lastname">
-      <el-input placeholder="Фамилия" v-model="user.lastname" class="text-input"></el-input>
+      <el-form-item label="Источники">
+        <el-input placeholder="■" v-model="event.firstname"></el-input>
+      </el-form-item>
+
+    <el-form-item label="Описание">
+      <el-input
+        type="textarea"
+        autosize
+        placeholder="Пастернак пишет письмо Александру Сергеевичу Пушкину, т.к. еще не знает о его трагической гибели на дуэли с Дантесом"
+        v-model="event.firstname">
+      </el-input>
+    </el-form-item>
+
+    <el-form-item label="Место">
+      <el-input placeholder="Пекин" v-model="event.firstname"></el-input>
+    </el-form-item>
+
+    <el-form-item label="Примечание">
+      <el-input
+        type="textarea"
+        autosize
+        placeholder="Был солнечный день"
+        v-model="event.firstname">
+      </el-input>
+    </el-form-item>
+
+    <el-form-item label="Участники">
+      <el-select
+          v-model="event.participants"
+          filterable
+          multiple
+          placeholder="Персоны"
+          >
+          <el-option
+            v-for="item in persons"
+            :key="item.id"
+            :label="item.value"
+            :value="item.id">
+          </el-option>
+        </el-select>
+    </el-form-item>
+
+    <el-form-item label="Упомянуты">
+        <el-select v-model="event.mentions" filterable multiple placeholder="Персоны">
+            <el-option
+              v-for="item in persons"
+              :key="item.id"
+              :label="item.value"
+              :value="item.id">
+            </el-option>
+          </el-select>
+    </el-form-item>
+
+    <el-form-item label="Произведения">
+        <el-select v-model="event.works" multiple filterable placeholder="Произведения">
+            <el-option
+              v-for="item in works"
+              :key="item.id"
+              :label="item.title"
+              :value="item.id">
+            </el-option>
+          </el-select>
+    </el-form-item>
+
+    <!-- :on-preview="handlePreview"
+    :on-remove="handleRemove"
+    :file-list="fileList" -->
+
+    <el-form-item prop="file" label="Медиафайл">
+      <el-upload
+        class="upload-demo"
+        drag
+        action="https://jsonplaceholder.typicode.com/posts/"
+        multiple
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">Перетащите файл сюда или <em>нажмите, чтобы загрузить</em></div>
+        <template #tip>
+          <div class="el-upload__tip">
+            Размер файла не больше 500кб
+          </div>
+        </template>
+      </el-upload>
     </el-form-item>
 
     <!-- <el-form-item>
@@ -38,15 +119,26 @@ export default {
   },
   setup() {
 
-    let event = ref({});
+    let event = ref({works: [], participants: [], mentions: []});
     const vuerouter = useRoute();
     const id = vuerouter.params.id;
+    const persons = ref([]);
+    const works = ref([]);
 
     onBeforeMount(async() => {
       const result = await store.getData("events", id);
       if("data" in result) {
         console.log("ok");
       }
+      const personsData = await store.getData("persons");
+      persons.value = personsData.data.map (x => ({...x, value: x.firstname + ' ' + x.lastname}) );
+      console.log(persons);
+
+      const resultWorks = await store.getData("works");
+      if("data" in resultWorks) {
+        works.value = resultWorks.data;
+      }
+
     });
 
     const onSubmit = async() => {
@@ -56,7 +148,7 @@ export default {
         console.log(result);
     };
 
-    return {onSubmit, event, options: store.state.options};
+    return {onSubmit, event, options: store.state.options, persons, works};
   },
   components: {
 

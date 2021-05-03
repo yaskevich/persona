7156af298068917b4f893fc2d53d158aee3ea6ff
yaskevich <alyaxey.yaskevich@gmail.com>
2021-05-03 13:25:47 +0300

    render timestamp in Fact [front]

--- front/src/components/Fact.vue
+++ front/src/components/Fact.vue
@@ -1,118 +1,109 @@
 <template>
-    <el-form label-width="120px" v-model="fact">
+   <el-form label-width="120px" v-model="fact">
+      <el-form-item prop="timestamp" label="">
+         <el-space  direction="horizontal" style="display:flex;" wrap size="large">
+            <el-date-picker v-model="fact.timestamp" type="datetime" placeholder="Техническое время" :shortcuts="shortcuts"></el-date-picker>
+            <!-- <el-input placeholder="переписка (БП_отправитель)" v-model="fact.firstname"></el-input> -->
+            <el-cascader :options="acts" clearable :props="{ multiple: true, }" placeholder="Вид(ы) деятельности"></el-cascader>
+         </el-space>
+      </el-form-item>
       <el-form-item prop="date" label="Дата">
-        <el-input placeholder="10.05.1928" v-model="fact.firstname" class="text-input" prop="firstName"></el-input>
+         <el-input placeholder="10.05.1928" v-model="fact.date" class="text-input" prop="date"></el-input>
       </el-form-item>
-      <el-form-item prop="tags" label="Деятельность">
-        <!-- <el-input placeholder="переписка (БП_отправитель)" v-model="fact.firstname"></el-input> -->
-        <el-cascader :options="acts" clearable :props="{ multiple: true, }" placeholder="Вид(ы) деятельности"></el-cascader>
-      </el-form-item>
-
       <el-form-item label="Источники">
-        <el-input placeholder="■" v-model="fact.firstname"></el-input>
+         <el-input placeholder="■" v-model="fact.firstname"></el-input>
       </el-form-item>
-
-    <el-form-item label="Описание">
-      <el-input
-        type="textarea"
-        autosize
-        placeholder="Пастернак пишет письмо Маяковскому"
-        v-model="fact.firstname">
-      </el-input>
-    </el-form-item>
-
-    <el-form-item label="Место">
-      <el-input placeholder="Пекин" v-model="fact.firstname"></el-input>
-    </el-form-item>
-
-    <el-form-item label="Примечание">
-      <el-input
-        type="textarea"
-        autosize
-        placeholder="Был солнечный день"
-        v-model="fact.firstname">
-      </el-input>
-    </el-form-item>
-
-    <el-form-item label="Связи">
-        <el-space  direction="horizontal" style="display:flex;" wrap size="large">
-      <el-select
-          v-model="fact.participants"
-          filterable
-          multiple
-          placeholder="Участники"
-          >
-          <el-option
-            v-for="item in persons"
-            :key="item.id"
-            :label="item.value"
-            :value="item.id">
-          </el-option>
-        </el-select>
-
-        <el-select v-model="fact.mentions" filterable multiple placeholder="Упомянуты">
-            <el-option
-              v-for="item in persons"
-              :key="item.id"
-              :label="item.value"
-              :value="item.id">
-            </el-option>
-          </el-select>
-
-        <el-select v-model="fact.works" multiple filterable placeholder="Произведения">
-            <el-option
-              v-for="item in works"
-              :key="item.id"
-              :label="item.title"
-              :value="item.id">
-            </el-option>
-          </el-select>
-
-        <el-select v-model="fact.books" multiple filterable placeholder="Издания">
-            <el-option
-              v-for="item in books"
-              :key="item.id"
-              :label="item.title"
-              :value="item.id">
-            </el-option>
-          </el-select>
-      </el-space>
-    </el-form-item>
-
-    <!-- :on-preview="handlePreview"
-    :on-remove="handleRemove"
-    :file-list="fileList" -->
-
-    <el-form-item prop="file" label="Медиафайл">
-      <el-upload
-        class="upload-demo"
-        drag
-        action="https://jsonplaceholder.typicode.com/posts/"
-        multiple
-      >
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">Перетащите файл сюда или <em>нажмите, чтобы загрузить</em></div>
-        <template #tip>
-          <div class="el-upload__tip">
-            Размер файла не больше 500кб
-          </div>
-        </template>
-      </el-upload>
-    </el-form-item>
-
-    <!-- <el-form-item>
-      <el-select v-model="user.privs" placeholder="Select" value-key>
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-    </el-form-item> -->
-
-     <el-button type="primary" @click="onSubmit">Сохранить</el-button>
-    </el-form>
+      <el-form-item label="Описание">
+         <el-input
+            type="textarea"
+            autosize
+            placeholder="Пастернак пишет письмо Маяковскому"
+            v-model="fact.firstname">
+         </el-input>
+      </el-form-item>
+      <el-form-item label="Место">
+         <el-input placeholder="Пекин" v-model="fact.firstname"></el-input>
+      </el-form-item>
+      <el-form-item label="Примечание">
+         <el-input
+            type="textarea"
+            autosize
+            placeholder="Был солнечный день"
+            v-model="fact.firstname">
+         </el-input>
+      </el-form-item>
+      <el-form-item label="Связи">
+         <el-space  direction="horizontal" style="display:flex;" wrap size="large">
+            <el-select
+               v-model="fact.participants"
+               filterable
+               multiple
+               placeholder="Участники"
+               >
+               <el-option
+                  v-for="item in persons"
+                  :key="item.id"
+                  :label="item.value"
+                  :value="item.id">
+               </el-option>
+            </el-select>
+            <el-select v-model="fact.mentions" filterable multiple placeholder="Упомянуты">
+               <el-option
+                  v-for="item in persons"
+                  :key="item.id"
+                  :label="item.value"
+                  :value="item.id">
+               </el-option>
+            </el-select>
+            <el-select v-model="fact.works" multiple filterable placeholder="Произведения">
+               <el-option
+                  v-for="item in works"
+                  :key="item.id"
+                  :label="item.title"
+                  :value="item.id">
+               </el-option>
+            </el-select>
+            <el-select v-model="fact.books" multiple filterable placeholder="Издания">
+               <el-option
+                  v-for="item in books"
+                  :key="item.id"
+                  :label="item.title"
+                  :value="item.id">
+               </el-option>
+            </el-select>
+         </el-space>
+      </el-form-item>
+      <!-- :on-preview="handlePreview"
+         :on-remove="handleRemove"
+         :file-list="fileList" -->
+      <el-form-item prop="file" label="Медиафайл">
+         <el-upload
+            class="upload-demo"
+            drag
+            action="https://jsonplaceholder.typicode.com/posts/"
+            multiple
+            >
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">Перетащите файл сюда или <em>нажмите, чтобы загрузить</em></div>
+            <template #tip>
+               <div class="el-upload__tip">
+                  Размер файла не больше 500кб
+               </div>
+            </template>
+         </el-upload>
+      </el-form-item>
+      <!-- <el-form-item>
+         <el-select v-model="user.privs" placeholder="Select" value-key>
+           <el-option
+             v-for="item in options"
+             :key="item.value"
+             :label="item.label"
+             :value="item.value">
+           </el-option>
+         </el-select>
+         </el-form-item> -->
+      <el-button type="primary" @click="onSubmit">Сохранить</el-button>
+   </el-form>
 </template>
 <script>
 import { ref } from 'vue';
@@ -126,8 +117,7 @@ export default {
     // datum: Object,
   },
   setup() {
-
-    let fact = ref({works: [], participants: [], mentions: [], books: []});
+    let fact = ref({ works: [], participants: [], mentions: [], books: [], date: '', timestamp: null, });
     const vuerouter = useRoute();
     const id = vuerouter.params.id;
     const persons = ref([]);
@@ -152,6 +142,17 @@ export default {
           },
 
         ];
+
+    const shortcuts = [{
+         text: '1920',
+         value: (() => new Date(1920, 1, 1, 9, 0, 0))(),
+      }, {
+         text: '1940',
+         value: (() => new Date(1940, 1, 1, 9, 0, 0))(),
+      }, {
+         text: '1960',
+         value: (() => new Date(1960, 1, 1, 9, 0, 0))(),
+      }];
 
     onBeforeMount(async() => {
       const result = await store.getData("facts", id);
@@ -181,7 +182,7 @@ export default {
         console.log(result);
     };
 
-    return {onSubmit, fact, options: store.state.options, persons, works, books, acts, };
+    return {onSubmit, fact, options: store.state.options, persons, works, books, acts, shortcuts, };
   },
   components: {
 

94c29bf5a370ccb4cf03a7329f171a4b4c62eebd
yaskevich <alyaxey.yaskevich@gmail.com>
2021-05-03 19:28:57 +0300

    handle dropdown in Acts [front]

--- front/src/components/Acts.vue
+++ front/src/components/Acts.vue
@@ -1,4 +1,26 @@
 <template>
+  <el-dialog
+  :title="dialogData.caption"
+  v-model="dialogData.visible"
+  width="30%"
+  :before-close="handleClose"
+  @opened="onDialogOpened">
+    <!-- <span>This is a message</span> -->
+    <el-input
+          ref="dialogInputRef"
+          placeholder=""
+          v-model="dialogData.text"
+          @keyup.enter="handleClose(Boolean(dialogData.text))"
+          >
+       </el-input>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="handleClose">Отмена</el-button>
+        <el-button type="primary" v-if="dialogData.text" @click="handleClose(true)">Сохранить</el-button>
+      </span>
+    </template>
+  </el-dialog>
+
   Типы событий
   <el-tree
     :data="data"
@@ -12,17 +34,22 @@
     @node-drop="handleDrop"
     draggable
     :allow-drop="allowDrop"
-    :allow-drag="allowDrag">
+    :allow-drag="allowDrag"
+    :expand-on-click-node="false"
+    style="max-width:350px;"
+    >
     <template #default="{ node, data }">
        <span class="custom-tree-node">
          <span>{{ node.label }}</span>
          <span>
-           <el-dropdown>
-             <i class="el-icon-link"></i>
+           <el-dropdown >
+             <i class="el-icon-s-tools"></i>
              <template #dropdown>
                <el-dropdown-menu>
-                   <el-dropdown-item>Добавить соседний пункт</el-dropdown-item>
-                   <el-dropdown-item>Удалить данный пункт</el-dropdown-item>
+                 <el-dropdown-item @click="addItem(node, data)">Добавить соседний пункт</el-dropdown-item>
+                 <el-dropdown-item @click="addChild(node, data)">Добавить вложенный пункт</el-dropdown-item>
+                 <el-dropdown-item @click="renameItem(node, data)">Переименовать</el-dropdown-item>
+                 <el-dropdown-item v-if="!data.children" @click="removeItem(node, data)"><strong>Удалить данный пункт</strong></el-dropdown-item>
                </el-dropdown-menu>
              </template>
            </el-dropdown>
@@ -44,6 +71,8 @@ export default defineComponent({
   setup() {
     const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
     const form  = reactive({ title: '', year: '', selectedWorks: [], selectedEditors: [], selectedAuthors: []});
+    const dialogData = reactive({ visible: false, text: "", caption: "", func: null, id: 0 });
+    const dialogInputRef = ref(null);
     // const works = ref([]);
     // let persons = [];
     // const loading = ref(false);
@@ -106,7 +135,7 @@ export default defineComponent({
       console.log(JSON.stringify(data));
     };
 
-    const data = [
+    let data = reactive([
       {
          label: 'Корреспонденция', id: 1,
          children: [
@@ -121,11 +150,82 @@ export default defineComponent({
            { label: 'Семья', id: 6 }
          ]
       },
+    ]);
 
-    ];
+    const addItem = (node, datum) => {
+      console.log(node, datum);
+      dialogData.visible = true;
+      dialogData.caption = "Добавить соседний пункт";
+      dialogData.id = node.id;
+      dialogData.func = function() {
+        const newChild = { id: id++, label: dialogData.text, children: [] };
+        if (!datum.children) {
+          datum.children = [];
+        }
+        if(node?.parent?.data?.children) {
+          node.parent.data.children.push(newChild);
+        } else {
+          data.push(newChild);
+        }
+      };
+    };
 
+    let id = 1000;
+
+    const addChild = (node, datum) => {
+      // console.log(node, datum);
+      dialogData.visible = true;
+      dialogData.id = node.id;
+      dialogData.caption = "Добавить вложенный пункт";
+      dialogData.func = function() {
+        const newChild = { id: id++, label: dialogData.text, children: [] };
+        if (!datum.children) {
+          datum.children = [];
+        }
+        datum.children.push(newChild);
+      };
+    };
+
+    const renameItem = (node, datum) => {
+      // console.log(node, datum);
+      dialogData.visible = true;
+      dialogData.caption = `Переименовать пункт «${datum.label}»`;
+      dialogData.id = node.id;
+      dialogData.func = function() {
+        const parent = node.parent;
+        const children = parent.data.children || parent.data;
+        const index = children.findIndex(d => d.id === datum.id);
+        children[index].label = dialogData.text;
+      };
+    };
+
+    const removeItem = (node, datum) => {
+      // console.log(node, datum);
+      if(datum.children) {
+        console.log("cannot delete non-empty slot!");
+      } else {
+        const parent = node.parent;
+        const children = parent.data.children || parent.data;
+        const index = children.findIndex(d => d.id === datum.id);
+        children.splice(index, 1);
+      }
+    };
+
+    const handleClose = (e) => {
+      // console.log("close", e);
+      dialogData.visible = false;
+      if (e === true) {
+        dialogData.func();
+        dialogData.text = "";
+      }
+    };
+
+    const onDialogOpened = () => {
+      dialogInputRef.value.focus();
+    }
 
     return {
+      onDialogOpened,
       confirm,
       handleDragStart,
       handleDragEnter,
@@ -141,7 +241,14 @@ export default defineComponent({
           label: 'label'
         },
       formRef,
-      form
+      form,
+      addItem,
+      addChild,
+      renameItem,
+      removeItem,
+      dialogData,
+      handleClose,
+      dialogInputRef,
     };
   },
 });

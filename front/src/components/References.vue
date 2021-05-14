ea8847ef9d96e95733dda2614bc7127cbe29cd54
yaskevich <alyaxey.yaskevich@gmail.com>
2021-05-14 23:36:46 +0300

    handle different types of nodes in References [front]

--- front/src/components/References.vue
+++ front/src/components/References.vue
@@ -1,22 +1,24 @@
 <template>
   <el-dialog
   title="Редактировать"
-  v-model="bibliodata.visible"
-  width="30%"
-  :before-close="handleClose"
-  @opened="onDialogOpened">
+  v-model="dialogVisible"
+  width="30%">
 
     <el-space>
-      <el-radio-group v-model="bibliodata.reftype">
-            <el-radio-button label="0">Сведения</el-radio-button>
-            <el-radio-button label="1">Заглавие</el-radio-button>
-            <el-radio-button label="2">Финаль</el-radio-button>
-          </el-radio-group>
+
+      <el-radio-group v-model="bib.reftype">
+        <el-radio-button label="0">Сведения</el-radio-button>
+        <el-radio-button label="1">Заглавие</el-radio-button>
+        <el-radio-button label="2">Финаль</el-radio-button>
+      </el-radio-group>
+
     </el-space>
+
     <div class="box">
-    <el-select
-        v-model="bibliodata.authors"
-        v-if="bibliodata.reftype==1"
+
+      <el-select
+        v-model="bib.authors"
+        v-if="bib.reftype==1"
         multiple
         filterable
         reserve-keyword
@@ -29,66 +31,61 @@
           :value="item.id">
         </el-option>
       </el-select>
-    </div>
-    <div v-if="bibliodata.reftype==2">
-    <div>
-    Страницы
-    </div>
-    <el-tag
-      :key="entry"
-      v-for="entry in bibliodata.pages"
-      closable
-      :disable-transitions="false"
-      @close="removePage(entry)"
-    >
-    {{entry}}
-  </el-tag>
-  <el-input
-    class="input-new-tag"
-    v-if="inputVisible"
-    v-model="page"
-    ref="saveTagInput"
-    size="mini"
-    @keyup.enter="handleInputConfirm"
-    @blur="handleInputConfirm"
-  >
-  </el-input>
-<el-button v-else class="button-new-tag" size="small" @click="showInput">+ добавить</el-button>
 
-    <el-space>
-
-      <!-- <el-input v-model="bibliodata.text"><template #prepend>С</template></el-input>
-      <el-input v-model="bibliodata.text"><template #prepend>По</template></el-input> -->
-      <!-- <el-input v-model="bibliodata.text"></el-input>
-      <el-button type="primary" icon="el-icon-plus"></el-button> -->
-    </el-space>
     </div>
 
+    <div v-if="bib.reftype==2">
+
+      <div>Страницы</div>
+      <el-tag
+        :key="entry"
+        v-for="entry in bib.pages"
+        closable
+        :disable-transitions="false"
+        @close="removePage(entry)">
+      {{entry}}
+      </el-tag>
+      <el-input
+        class="input-new-tag"
+        v-if="inputVisible"
+        v-model="page"
+        ref="saveTagInput"
+        size="mini"
+        @keyup.enter="handleInputConfirm"
+        @blur="handleInputConfirm">
+      </el-input>
+      <el-button v-else class="button-new-tag" size="small" @click="showInput">+ добавить</el-button>
+      <el-space>
+        <!-- <el-input v-model="bib.text"><template #prepend>С</template></el-input>
+        <el-input v-model="bib.text"><template #prepend>По</template></el-input> -->
+        <!-- <el-input v-model="bib.text"></el-input>
+        <el-button type="primary" icon="el-icon-plus"></el-button> -->
+      </el-space>
+
+    </div>
 
     <div class="box">
-      <span v-if="bibliodata.reftype==0">Запись</span>
-      <span v-if="bibliodata.reftype==1">Название</span>
-      <span v-if="bibliodata.reftype==2">Название-описание</span>
+
+      <span v-if="bib.reftype==0">Запись</span>
+      <span v-if="bib.reftype==1">Название</span>
+      <span v-if="bib.reftype==2">Название-описание</span>
 
     </div>
-    <el-input
-          placeholder=""
-          v-model="bibliodata.title"
-          @keyup.enter="handleClose(Boolean(bibliodata.title))"
-          >
-    </el-input>
 
-    <div v-if="bibliodata.reftype==2">
-          <div class="box">Цитата</div>
-          <el-input v-model="bibliodata.content" type="textarea"></el-input>
+    <el-input v-model="bib.title"></el-input>
+
+    <div v-if="bib.reftype==2">
+      <div class="box">Цитата</div>
+      <el-input v-model="bib.content" type="textarea"></el-input>
     </div>
 
     <template #footer>
       <span class="dialog-footer">
-        <el-button @click="handleClose">Отмена</el-button>
-        <el-button type="primary" v-if="bibliodata.title" @click="handleClose(true)">Сохранить</el-button>
+        <el-button @click="dialogVisible = false;">Отмена</el-button>
+        <el-button type="primary" v-if="bib.title" @click="handleClose(true)">Сохранить</el-button>
       </span>
     </template>
+
   </el-dialog>
 
   <div v-if="!isEmbedded">
@@ -118,9 +115,9 @@
              <i class="el-icon-s-tools"></i>
              <template #dropdown>
                <el-dropdown-menu>
-                 <!-- <el-dropdown-item @click="addItem(node, data)">Добавить соседний пункт</el-dropdown-item>
-                 <el-dropdown-item @click="addItem(node, data, true)">Добавить вложенный пункт</el-dropdown-item> -->
-                 <el-dropdown-item @click="renameItem(node, data)">Редактировать</el-dropdown-item>
+                 <el-dropdown-item @click="buildDialog(node, data, 1)">Добавить соседний пункт</el-dropdown-item>
+                 <el-dropdown-item v-if="data.reftype != 2" @click="buildDialog(node, data, 2)">Добавить вложенный пункт</el-dropdown-item>
+                 <el-dropdown-item @click="buildDialog(node, data)">Редактировать</el-dropdown-item>
                  <!-- <el-dropdown-item v-if="!data?.children?.length" @click="removeItem(node, data)"><strong>Удалить этот пункт</strong></el-dropdown-item> -->
                </el-dropdown-menu>
              </template>
@@ -133,8 +130,7 @@
 
 
 <script lang="ts">
-import { defineComponent, ref, reactive, onBeforeMount, ComponentPublicInstance, toRaw } from 'vue';
-import { ElForm } from 'element-plus';
+import { defineComponent, ref, reactive, onBeforeMount, toRaw } from 'vue';
 import store from "../store";
 
 export default defineComponent({
@@ -142,49 +138,16 @@ export default defineComponent({
     isEmbedded: Boolean,
   },
   setup() {
-    const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
-    const bibliodata = reactive({ visible: false, title: "", content: "", id: 0 , reftype: 0, pages: [], authors: [], parent: null });
+    const scheme  = { title: "", content: "", id: null, reftype: 0, pages: [], authors: [], parent: null, mode: 0 };
+    const bib = reactive({});
+    Object.assign(bib, scheme);
+    const dialogVisible  = ref(false);
     const refs = reactive([]);
     const authors = ref([]);
     const loading = ref(false);
-    let persons = [];
     const inputVisible = ref(false);
     const page = ref('');
-
-
-    // const refs =  [
-    //           {
-    //              title: 'Полное собрание сочинений', id: 1,
-    //              children: [
-    //                { title: 'Том 8', id: 2, children: [
-    //                  { title: 'Часть 1', id: 6,  children: [
-    //                    { title: 'М.: СЛОВО/SLOVO, 2004 г.', id: 13, children: [
-    //                       { title: 'C. 115', id: 14 }
-    //                    ] }
-    //                  ]
-    //                }
-    //                ]
-    //                },
-    //              ]
-    //           },
-    //           {
-    //              title: 'Уч. зап. Тартус. гос. ун-та. Труды по рус. и слав. фил. IX. Литературоведение.', id: 4,
-    //              children: [
-    //                { title: 'Вып. 184', id: 6, children: [{ title: 'C. 284', id: 24 }] }
-    //              ]
-    //           },
-    //
-    //         ];
-
-    // const loading = ref(false);
-
-    const getAuthors = (query) => {
-      console.log(query);
-      const re  = new RegExp(query, "i");
-      loading.value = true;
-      authors.value = persons.filter(x => re.test(x.value));
-      loading.value = false;
-    };
+    let nodeRef = {};
 
     onBeforeMount(async() => {
       const result = await store.getData("refs");
@@ -199,78 +162,77 @@ export default defineComponent({
       console.log("authors", authors);
     });
 
-    const renameItem = (node, datum) => {
-      // console.log(node, datum);
-      bibliodata.visible = true;
-      bibliodata.id = datum.id;
-      bibliodata.title = datum.title;
+    const buildDialog = (node, datum, mode) => {
+      console.log("node", node);
+      nodeRef = node;
+      Object.assign(bib, mode ? scheme: datum, { mode: mode });
+      dialogVisible.value = true;
     };
 
-    const handleClose = (e) => {
-      // console.log("close", e);
-      bibliodata.visible = false;
-      // authors: null
-      // content: null
-      // id: 1
-      // pages: null
-      // parent: null
-      // reftype: 0
-      // title: "Полное собрание сочинений. В 11 томах"
-      if (e === true) {
+    const handleClose = async(e) => {
+      console.log("close", e);
+      dialogVisible.value = false;
 
 
-        console.log("reftype", bibliodata.reftype);
-        if (bibliodata.reftype == 0) {
-          console.log("check 0");
-          bibliodata.authors = null;
-          bibliodata.content = null;
-          bibliodata.pages = [];
-        } else if (bibliodata.reftype == 1) {
-          console.log("check 1");
-          bibliodata.content = null;
-          bibliodata.pages = [];
+      if (bib.reftype == 0) {
+        console.log("check 0");
+        bib.authors = null;
+        bib.content = null;
+        bib.pages = [];
+      } else if (bib.reftype == 1) {
+        console.log("check 1");
+        bib.content = null;
+        bib.pages = [];
+      }
+
+        const datum = toRaw(bib);
+        delete datum['children'];
+        delete datum['mode'];
+
+        if (datum.id) {
+          console.log("UPDATE NODE", datum);
+          for (let prop in datum) {
+            nodeRef.data[prop] = datum[prop];
+          }
+          // const result = await store.postData("refs", datum);
+          // console.log(result);
+
+        } else {
+          // take id from server
+          console.log("mode", (bib.mode == 2 ? "child": "neighbor"));
+          console.log("node ref", nodeRef);
         }
 
-        const datum = toRaw(bibliodata);
-        console.log("result", datum);
-      }
-    };
-
-    const onDialogOpened = () => {
+        console.log("result", nodeRef);
     };
 
     const showInput = () => {
         inputVisible.value = true;
     };
 
-
     const removePage = (entry) => {
-      bibliodata.pages.splice(bibliodata.pages.indexOf(entry), 1);
+      bib.pages.splice(bib.pages.indexOf(entry), 1);
     };
 
     const handleInputConfirm = () => {
         if (page.value) {
-          bibliodata.pages.push(page.value);
+          bib.pages ? bib.pages.push(page.value) : bib.pages = [page.value];
         }
         inputVisible.value = false;
         page.value = '';
     };
 
     return {
+      dialogVisible,
       handleInputConfirm,
       removePage,
       page,
       inputVisible,
       showInput,
-      onDialogOpened,
-      formRef,
-      // addItem,
-      renameItem,
-      // removeItem,
-      bibliodata,
+      buildDialog,
+      bib,
       handleClose,
       refs,
-      getAuthors,
       authors,
       loading,
     };

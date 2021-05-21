441e93d0235eba8bee36d9670d1aa551924db6ed
yaskevich <alyaxey.yaskevich@gmail.com>
2021-05-22 00:09:28 +0300

    enable Russian locale for Element UI [front]

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,9 +3,11 @@ import App from './App.vue'
 import "../tailwind.css"
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
+import locale from 'element-plus/lib/locale/lang/ru'
+import 'dayjs/locale/ru'
 import router from './router'
 
 createApp(App)
 .use(router)
-.use(ElementPlus)
+.use(ElementPlus, { locale })
 .mount('#app');

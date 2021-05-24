fad62eaaedd73399b50dd44464671172faef96f1
yaskevich <alyaxey.yaskevich@gmail.com>
2021-05-24 17:12:11 +0300

    handle user activation status in UserList [front]

--- front/src/components/UserList.vue
+++ front/src/components/UserList.vue
@@ -31,18 +31,28 @@
 <div style="margin-top:2rem;"></div>
   <el-row v-for="(value, key) in users"  :gutter="20" :key="key">
     <el-col :span="4"><div class="grid-content bg-purple"><i :class="value.sex === 1 ? 'el-icon-male' : 'el-icon-female'"></i> {{value.firstname}}</div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple">{{value.lastname}}</div></el-col>
+    <el-col :span="4"><div class="grid-content bg-purple">{{value.lastname}}</div></el-col>
     <el-col :span="4"><div class="grid-content bg-purple-light">{{options.filter(x => x.value === value.privs)[0]["label"].split('(')[0]}}</div></el-col>
     <el-col :span="4"><div class="grid-content bg-purple">
       <router-link :to="'/user/' + value.id">
       <el-button type="text" size="mini" icon="el-icon-edit" plain class="full-width"></el-button>
     </router-link>
-    </div></el-col>
+    </div>
+    </el-col>
+    <el-col :span="4">
+      <div v-if="value.privs == 1 || me.id === value.id ">
+        <i class="el-icon-check"></i>
+      </div>
+      <div v-else>
+        <div v-if="value.activated" class="grid-content bg-purple-light"><el-button type="danger" @click="switchUserStatus(value)">Отключить</el-button></div>
+        <div v-else class="grid-content bg-purple-light"><el-button @click="switchUserStatus(value)">Активировать</el-button></div>
+      </div>
+    </el-col>
   </el-row>
 </template>
 
 <script lang="ts">
-import { ElForm } from 'element-plus';
+import { ElForm, ElMessageBox,   } from 'element-plus';
 import { defineComponent, ref, reactive, onBeforeMount, ComponentPublicInstance } from 'vue';
 import store from "../store";
 
@@ -56,24 +66,38 @@ export default defineComponent({
       const result = await store.getData("users");
       // if (response.data && Object.keys(response.data).length) {
       if("data" in result) {
-        users.push(...result.data);
-      } else {
-        form.privs = 1; // create admin on first run
+        users.push(...result.data.sort((a, b) => b.id - a.id));
       }
     });
+
+    const switchUserStatus = async(user) => {
+      console.log(user);
+      user.activated  = !user.activated;
+      const result = await store.postData("users", user);
+      console.log(result);
+    };
 
     const resetForm = () => {
       formRef.value?.resetFields();
     };
+
     const confirm = () => {
       formRef.value?.validate(async(valid) => {
-
         if (valid) {
           // do
           console.log("ok send", form);
           // return false;
           const result = await store.initUser(form);
           console.log(result);
+          if("data" in result && "message" in result.data) {
+            ElMessageBox({
+                  message: result.data.message,
+                  title: `Пароль для пользователя ${form.firstname} ${form.lastname} `,
+                  type: "success",
+                  showClose: true,
+                  center: true
+                });
+          }
           users.unshift({...form});
           formRef.value?.resetFields();
         }  else{
@@ -99,13 +123,15 @@ export default defineComponent({
     };
 
     return {
+      switchUserStatus,
       resetForm,
       confirm,
       formRef,
       rules,
       form,
       users,
-      options: store.state.options
+      options: store.state.options,
+      me: store.state.user,
     };
   },
 });

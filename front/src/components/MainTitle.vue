038735e02dab7a51b57147d22c3bc7fac493adfe
yaskevich <alyaxey.yaskevich@gmail.com>
2021-05-26 12:26:24 +0300

    create component Maintitle [front]

---
+++ front/src/components/MainTitle.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-row type="flex" justify="center">
+    <el-space size="medium" :spacer="spacer">
+      <h3>{{title}}</h3>
+      <el-button v-if="callback" size="medium" type="info" plain @click="callback">{{text}}</el-button>
+    </el-space>
+  </el-row>
+</template>
+
+<script lang="ts">
+import { ElDivider } from 'element-plus';
+import { defineComponent, h } from 'vue';
+
+export default defineComponent({
+  props: {
+      title: String,
+      callback: Function,
+      text: {
+        type: String,
+        default: "Добавить"
+      },
+  },
+
+  setup() {
+    return {spacer: h(ElDivider, { direction: 'vertical' }),}
+  }
+});
+
+</script>

ffe8ba7ff14a0dc9194248eb6ed4a7f02685e50c
yaskevich <alyaxey.yaskevich@gmail.com>
2021-07-17 17:08:28 +0300

    handle filtering in PersonList [front]

--- front/src/components/PersonList.vue
+++ front/src/components/PersonList.vue
@@ -1,136 +1,167 @@
 <template>
-<MainTitle title="Персоналии" :callback="confirm"></MainTitle>
-<!-- ref="formInstance" -->
-<el-form label-width="120px" ref="formRef" :model="form" :inline="true" :rules="rules">
-  <!-- label="Имя" -->
-  <el-form-item prop="firstname">
-    <el-input placeholder="Имя" v-model="form.firstname" class="text-input"></el-input>
-  </el-form-item>
-  <el-form-item prop="patroname">
-    <el-input placeholder="Отчество" v-model="form.patroname" class="text-input"></el-input>
-  </el-form-item>
-  <el-form-item prop="lastname">
-    <el-input placeholder="Фамилия" v-model="form.lastname"></el-input>
-  </el-form-item>
-<!-- <el-input placeholder="Фамилия при рождении" v-model="form.lastName2" class="text-input"></el-input> -->
-  <el-form-item prop="wikidata">
-    <el-input placeholder="Wikidata ID" prop="wikidata" v-model="form.wikidata" class="text-input"></el-input>
-  </el-form-item>
-<!-- <el-form-item label="Пол"> -->
-<el-form-item>
-  <el-radio-group v-model="form.sex">
-    <el-radio label="1">Мужчина</el-radio>
-    <el-radio label="2">Женщина</el-radio>
-  </el-radio-group>
-</el-form-item>
-</el-form>
 
-<el-row v-for="(value, key) in persons"  :gutter="20" :key="key">
-  <el-col :span="4"><div class="grid-content bg-purple"><i :class="value.sex === 1 ? 'el-icon-male' : 'el-icon-female'"></i> {{value.firstname}}</div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple-light">{{value.patroname}}</div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple">{{value.lastname}}</div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple-light">{{value.wikidata||'&nbsp;'}}</div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple">
-    <router-link :to="'/person/' + value.id">
-    <el-button type="text" size="mini" icon="el-icon-edit" plain class="full-width"></el-button>
-  </router-link>
-  </div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple-light">
-    <!-- <el-button type="primary" @click="confirm">Удалить</el-button> -->
-    <el-popconfirm
-      title="Точно удалить?"
-      confirmButtonText="Да!"
-      cancelButtonText="Нет"
-      @confirm="deletePerson(value.id, key)"
-    >
-    <template #reference>
-      <el-button>Удалить</el-button>
-      </template>
-      </el-popconfirm>
+  <MainTitle title="Персоналии" :callback="confirm"></MainTitle>
 
-  </div></el-col>
-</el-row>
+  <el-form label-width="120px" ref="formRef" :model="form" :inline="true" :rules="rules">
+
+    <el-form-item prop="firstname">
+      <el-input placeholder="Имя" v-model="form.firstname" class="text-input"></el-input>
+    </el-form-item>
+
+    <el-form-item prop="patroname">
+      <el-input placeholder="Отчество" v-model="form.patroname" class="text-input"></el-input>
+    </el-form-item>
+
+    <el-form-item prop="lastname">
+      <el-input placeholder="Фамилия" v-model="form.lastname"></el-input>
+    </el-form-item>
+
+    <el-form-item prop="wikidata">
+      <el-input placeholder="Wikidata ID" prop="wikidata" v-model="form.wikidata" class="text-input" style="max-width:110px;"></el-input>
+    </el-form-item>
+
+    <el-form-item>
+      <el-radio-group v-model="form.sex">
+        <el-radio label="1">Мужчина</el-radio>
+        <el-radio label="2">Женщина</el-radio>
+      </el-radio-group>
+    </el-form-item>
+
+  </el-form>
+
+  <el-row type="flex" justify="center">
+
+    <el-input placeholder="Фильтр по ФИО"
+              v-model="filterString"
+              style="max-width: 280px;"
+              clearable>
+    </el-input>
+
+  </el-row>
+
+  <el-row v-for="(value, key) in filtered()" :gutter="20" :key="key">
+
+    <el-col :span="4">
+      <div class="grid-content bg-purple"><i :class="value.sex === 1 ? 'el-icon-male' : 'el-icon-female'"></i> {{value.firstname}}</div>
+    </el-col>
+
+    <el-col :span="4">
+      <div class="grid-content bg-purple-light">{{value.patroname}}</div>
+    </el-col>
+
+    <el-col :span="4">
+      <div class="grid-content bg-purple">{{value.lastname}}</div>
+    </el-col>
+
+    <el-col :span="4">
+      <div class="grid-content bg-purple-light">{{value.wikidata||'&nbsp;'}}</div>
+    </el-col>
+
+    <el-col :span="4">
+      <div class="grid-content bg-purple">
+        <router-link :to="'/person/' + value.id">
+          <el-button type="text" size="mini" icon="el-icon-edit" plain class="full-width"></el-button>
+        </router-link>
+      </div>
+    </el-col>
+
+    <el-col :span="4">
+      <div class="grid-content bg-purple-light">
+        <!-- <el-button type="primary" @click="confirm">Удалить</el-button> -->
+        <el-popconfirm title="Точно удалить?"
+                       confirmButtonText="Да!"
+                       cancelButtonText="Нет"
+                       @confirm="deletePerson(value.id, key)">
+          <template #reference>
+            <el-button>Удалить</el-button>
+            </template>
+        </el-popconfirm>
+      </div>
+    </el-col>
+
+  </el-row>
+
 </template>
 
 <script lang="ts">
-import { ElForm } from 'element-plus';
-import { defineComponent, ref, reactive, onBeforeMount, ComponentPublicInstance } from 'vue';
-import store from "../store";
-import MainTitle from './MainTitle.vue'
 
-export default defineComponent({
-  setup() {
-    const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
-    const form  = reactive({ firstname: '', lastname: '', patroname: '', sex: '1', wikidata: ''});
-    const persons = reactive([]);
+  import { ElForm } from 'element-plus';
+  import { defineComponent, ref, reactive, onBeforeMount, ComponentPublicInstance } from 'vue';
+  import store from '../store';
+  import MainTitle from './MainTitle.vue';
 
-    onBeforeMount(async() => {
-      const result = await store.getData("persons");
-      if("data" in result) {
-        persons.push(...result.data.sort((a, b) => b.id - a.id));
-      }
-    });
+  export default defineComponent({
+    setup() {
+      const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
+      const form = reactive({ firstname: '', lastname: '', patroname: '', sex: '1', wikidata: '' });
+      const persons = reactive([]);
+      const filterString = ref('');
 
-    // const isString = (rule, value, callback) => {
-    //   // console.log("x", rule, value);
-    //   const val = form[rule.field];
-    //   if(val) {
-    //     const res = form[rule.field].match(/^[а-я-]+$/gi);
-    //     // console.log("res", form[rule.field], res);
-    //     if(!res) {
-    //       return  callback(new Error('Не соответствует формату')) ;
-    //     }
-    //   } else {
-    //     return  callback(new Error('Обязательное поле')) ;
-    //   }
-    // };
-    const deletePerson = async(id, key) => {
-      const result = await store.deleteById("persons", id);
-      // console.log(result);
-      if("data" in result && "id" in result.data) {
-        console.log("deleted", id);
-        persons.splice(key, 1);
-      }
-    };
-    const resetForm = () => {
-      formRef.value?.resetFields();
-    };
-    const confirm = () => {
-      formRef.value?.validate(async(valid) => {
-        if (valid) {
-          console.log("ok send", form);
-          const result = await store.postData("persons", form);
-          console.log(result);
-          if("data" in result && "id" in result.data) {
-            persons.unshift({...form});
-            console.log("success", persons);
-            formRef.value?.resetFields();
-          }
-        }  else{
-          console.log("form not valid");
-          return false;
+      onBeforeMount(async () => {
+        const { data } = await store.getData('persons');
+        if (data) {
+          persons.push(...data.sort((a, b) => b.id - a.id));
         }
       });
-    };
 
-    const  rules = {
-      firstname: [
-        { required: true, message: 'Поле должно быть заполнено', trigger: 'blur' },
-        { min: 2, message: 'Не менее 2-х символов', trigger: 'blur' }
-      ],
-      lastname: [
-        { required: true, message: 'Поле должно быть заполнено', trigger: 'blur' },
-        { min: 2, message: 'Не менее 2-х символов', trigger: 'blur' }
-      ],
-      patroname: [
-        { required: false, message: 'Поле должно быть заполнено', trigger: 'blur' },
-        { min: 2, message: 'Не менее 2-х символов', trigger: 'blur' }
-      ],
-    };
-    return { deletePerson, resetForm, confirm, formRef, form, persons, rules};
-  },
-  components: {
-    MainTitle,
-  }
-});
+      const deletePerson = async (id, key) => {
+        const { data } = await store.deleteById('persons', id);
+        if (data && 'id' in data) {
+          // console.log('deleted', id);
+          persons.splice(key, 1);
+        }
+      };
+
+      const resetForm = () => {
+        formRef.value?.resetFields();
+      };
+
+      const confirm = () => {
+        formRef.value?.validate(async valid => {
+          if (valid) {
+            const { data } = await store.postData('persons', form);
+            // console.log(result);
+            if (data && 'id' in data) {
+              persons.unshift({ ...form });
+              // console.log('success', persons);
+              formRef.value?.resetFields();
+            }
+          } else {
+            // console.log('form not valid');
+            return false;
+          }
+        });
+      };
+
+      const rules = {
+        firstname: [
+          { required: true, message: 'Поле должно быть заполнено', trigger: 'blur' },
+          { min: 2, message: 'Не менее 2-х символов', trigger: 'blur' },
+        ],
+        lastname: [
+          { required: true, message: 'Поле должно быть заполнено', trigger: 'blur' },
+          { min: 2, message: 'Не менее 2-х символов', trigger: 'blur' },
+        ],
+        patroname: [
+          { required: false, message: 'Поле должно быть заполнено', trigger: 'blur' },
+          { min: 2, message: 'Не менее 2-х символов', trigger: 'blur' },
+        ],
+      };
+
+      const filtered = () => {
+        const re = new RegExp(filterString.value, 'i');
+        return filterString.value
+          ? persons.filter(
+              x => x.firstname.search(re) !== -1 || x.lastname.search(re) !== -1 || x.patroname.search(re) !== -1
+            )
+          : persons;
+      };
+
+      return { deletePerson, resetForm, confirm, formRef, form, persons, rules, filterString, filtered };
+    },
+    components: {
+      MainTitle,
+    },
+  });
+
 </script>

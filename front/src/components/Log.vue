e24314943033c9ca2dd115e1e7e7bb58a5face9d
yaskevich <alyaxey.yaskevich@gmail.com>
2021-07-19 00:17:34 +0300

    localize Log [front]

--- front/src/components/Log.vue
+++ front/src/components/Log.vue
@@ -1,9 +1,9 @@
 <template>
-  <el-descriptions class="margin-top" title="Запись в базу данных" :column="2" border>
+  <el-descriptions class="margin-top" :title="store.texts['dbevent'][store.state.lang]" :column="2" border>
       <el-descriptions-item>
         <template #label>
           <i class="el-icon-user"></i>
-          Пользователь
+          {{store.texts['user'][store.state.lang]}}
         </template>
         {{user["firstname"] + ' ' + user["lastname"]}}
       </el-descriptions-item>
@@ -11,7 +11,7 @@
       <el-descriptions-item>
         <template #label>
           <i class="el-icon-mobile-phone"></i>
-          Действие
+          {{store.texts['event'][store.state.lang]}}
         </template>
         {{store.classify(record)}}
       </el-descriptions-item>
@@ -27,15 +27,17 @@
       <el-descriptions-item>
         <template #label>
           <i class="el-icon-tickets"></i>
-          Тип данных
+          {{store.texts['datatype'][store.state.lang]}}
         </template>
-        <el-tag>{{record.table_name}}</el-tag>
+        <el-tag>
+          {{store.texts?.[record.table_name]?.[store.state.lang]}}
+        </el-tag>
       </el-descriptions-item>
 
       <el-descriptions-item>
         <template #label>
           <i class="el-icon-office-building"></i>
-          Дата и время
+        {{store.texts['dateandtime'][store.state.lang]}}
         </template>
         {{store.dateToString(record.created)}}
       </el-descriptions-item>
@@ -43,22 +45,24 @@
     </el-descriptions>
 
 
-<h3 class="el-descriptions__title" style="margin-top:2rem;">Данные</h3>
+<h3 class="el-descriptions__title" style="margin-top:2rem;">
+  {{store.texts['data'][store.state.lang]}}
+</h3>
 <hr/>
 <el-row  :gutter="20">
   <el-col :span="8">
     <div class="grid-content el-descriptions__title bg-purple-light">
-      Поле
+      {{store.texts['field'][store.state.lang]}}
     </div>
   </el-col>
   <el-col :span="8">
     <div class="grid-content el-descriptions__title bg-purple">
-      Было
+      {{store.texts['wasbefore'][store.state.lang]}}
     </div>
   </el-col>
   <el-col :span="8">
     <div class="grid-content el-descriptions__title bg-purple-light">
-      Стало
+      {{store.texts['becameafter'][store.state.lang]}}
     </div>
   </el-col>
 </el-row>
@@ -87,7 +91,6 @@ import { ref } from 'vue';
 import { onBeforeMount } from 'vue';
 import store from "../store";
 import { useRoute } from 'vue-router';
-import router from "../router";
 
 export default {
   name: "Log",

f5133ad0f2ee1e8cfffe4813cc3d8d338e2cdcb3
yaskevich <alyaxey.yaskevich@gmail.com>
2021-08-11 23:46:46 +0300

    localize Login [front]

--- front/src/components/Login.vue
+++ front/src/components/Login.vue
@@ -1,67 +1,77 @@
 <template>
+
   <el-form label-width="120px" ref="formRef" :model="user" :inline="true" :rules="rules">
     <el-form-item prop="email">
       <el-input placeholder="E-mail" v-model="user.email"></el-input>
     </el-form-item>
     <el-form-item prop="password">
-      <el-input type="password" placeholder="Пароль" v-model="user.password" autocomplete="off"></el-input>
+      <el-input type="password" :placeholder="loc('pswd')" v-model="user.password" autocomplete="off"></el-input>
     </el-form-item>
-    <el-button type="primary" @click="confirm">Войти</el-button>
+    <el-button type="primary" @click="confirm">{{loc('login')}}</el-button>
     <div class="error">{{error}}</div>
   </el-form>
+
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive, ComponentPublicInstance } from 'vue';
-import { ElForm } from 'element-plus';
-import store from "../store";
 
-export default defineComponent({
-  setup() {
-    const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
-    let user = reactive({email: '', password: ''});
-    const error = ref('');
+  import { defineComponent, ref, reactive, ComponentPublicInstance } from 'vue';
+  import { ElForm } from 'element-plus';
+  import store from '../store';
 
-    // const resetForm = () => {
-    //   formRef.value?.resetFields();
-    // };
-    const confirm = async () => {
-      formRef.value?.validate(async(valid) => {
-        if (valid) {
-          console.log("ok send", user);
-          // return false;
-          error.value  = await store.doLogin(user);
-          // if(!error.value) {
-          //     // formRef.value?.resetFields();
-          // }
-          console.log(store.state);
-        }  else{
-          console.log("not valid");
-          return false;
-        }
-      });
-    };
+  export default defineComponent({
+    setup() {
+      const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
+      let user = reactive({ email: '', password: '' });
+      const error = ref('');
 
+      // const resetForm = () => {
+      //   formRef.value?.resetFields();
+      // };
+      const confirm = async () => {
+        formRef.value?.validate(async valid => {
+          if (valid) {
+            // console.log('ok send', user);
+            // return false;
+            error.value = await store.doLogin(user);
+            // if(!error.value) {
+            //     // formRef.value?.resetFields();
+            // }
+          } else {
+            console.log('not valid');
+            return false;
+          }
+        });
+      };
 
-    const  rules = {
-      email: [
-          { required: true, message: 'Поле должно быть заполнено', trigger: 'blur' },
-        { type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change'] }
-      ],
-      password: [
-        { required: true, message: 'Поле должно быть заполнено', trigger: 'blur' },
-        { min: 18, message: 'Не менее 18 символов', trigger: 'blur' }
-      ],
-    };
+      const rules = {
+        email: [
+          { required: true, message: store.loc('fieldnonempty'), trigger: 'blur' },
+          { type: 'email', message: store.loc('fieldemail'), trigger: ['blur', 'change'] },
+        ],
+        password: [
+          { required: true, message: store.loc('fieldnonempty'), trigger: 'blur' },
+          { min: 18, message: store.loc('pswdmore'), trigger: 'blur' },
+        ],
+      };
 
+      return {
+        formRef,
+        confirm,
+        user,
+        rules,
+        error,
+        loc: store.loc,
+      };
+    },
+  });
 
-    return { formRef, confirm, user, rules, error };
-  },
-});
 </script>
 
 <style scoped>
-.error {
-  color: red;
-}
+
+  .error {
+    color: red;
+  }
+
 </style>

5ae0b148150c527c69f237a6ce0b394bb758b5ad
yaskevich <alyaxey.yaskevich@gmail.com>
2021-08-11 23:47:12 +0300

    localize MainTitle [front]

--- front/src/components/MainTitle.vue
+++ front/src/components/MainTitle.vue
@@ -1,29 +1,33 @@
 <template>
+
   <el-row type="flex" justify="center">
     <el-space size="medium" :spacer="spacer">
       <h3>{{title}}</h3>
-      <el-button v-if="callback" size="medium" type="info" plain @click="callback">{{text}}</el-button>
+      <el-button v-if="callback" size="medium" type="info" plain @click="callback">{{text||loc('add')}}</el-button>
     </el-space>
   </el-row>
+
 </template>
 
 <script lang="ts">
-import { ElDivider } from 'element-plus';
-import { defineComponent, h } from 'vue';
 
-export default defineComponent({
-  props: {
+  import { ElDivider } from 'element-plus';
+  import { defineComponent, h } from 'vue';
+  import store from '../store';
+
+  export default defineComponent({
+    props: {
       title: String,
       callback: Function,
-      text: {
-        type: String,
-        default: "Добавить"
-      },
-  },
+      text: String,
+    },
 
-  setup() {
-    return {spacer: h(ElDivider, { direction: 'vertical' }),}
-  }
-});
+    setup() {
+      return {
+        spacer: h(ElDivider, { direction: 'vertical' }),
+        loc: store.loc,
+      };
+    },
+  });
 
 </script>

8123b1ea78d22c98caed0f6af9fc9da73fea7b92
yaskevich <alyaxey.yaskevich@gmail.com>
2021-08-12 23:31:39 +0300

    localize Person [front]

--- front/src/components/Person.vue
+++ front/src/components/Person.vue
@@ -1,28 +1,26 @@
 <template>
 
-  <MainTitle :title="'Персона ' + $route.params.id " :callback="confirm" text="Сохранить"></MainTitle>
+  <MainTitle :title="loc('person') + ' ' + $route.params.id " :callback="confirm" :text="loc('save')"></MainTitle>
 
   <el-form label-width="120px" v-model="person">
-    <!-- label="Имя" -->
-    <el-form-item prop="firstname" label="Имя">
-      <el-input placeholder="Имя" v-model="person.firstname" class="text-input" prop="firstName"></el-input>
+    <el-form-item prop="firstname" :label="loc('firstname')">
+      <el-input :placeholder="loc('firstname')" v-model="person.firstname" class="text-input" prop="firstName"></el-input>
     </el-form-item>
-    <el-form-item prop="patroname" label="Отчество">
-      <el-input placeholder="Отчество" v-model="person.patroname" class="text-input"></el-input>
+    <el-form-item prop="patroname" :label="loc('patroname')">
+      <el-input :placeholder="loc('patroname')" v-model="person.patroname" class="text-input"></el-input>
     </el-form-item>
-    <el-form-item prop="lastname" label="Фамилия">
-      <el-input placeholder="Фамилия" v-model="person.lastname" class="text-input"></el-input>
+    <el-form-item prop="lastname" :label="loc('lastname')">
+      <el-input :placeholder="loc('lastname')" v-model="person.lastname" class="text-input"></el-input>
     </el-form-item>
-    <!-- <el-input placeholder="Фамилия при рождении" v-model="form.lastName2" class="text-input"></el-input> -->
 
-    <el-form-item prop="wikidata" label="Wikidata ID">
-      <el-input placeholder="Wikidata ID" v-model="person.wikidata" class="text-input"></el-input>
+    <el-form-item prop="wikidata" :label="loc('wikidata')">
+      <el-input :placeholder="loc('wikidata')" v-model="person.wikidata" class="text-input"></el-input>
     </el-form-item>
-    <!-- <el-form-item label="Пол"> -->
+    <!-- <el-form-item :label="loc('sex')"> -->
     <el-form-item>
       <el-radio-group v-model="person.sex">
-        <el-radio label="1">Мужчина</el-radio>
-        <el-radio label="2">Женщина</el-radio>
+        <el-radio label="1">{{loc('man')}}</el-radio>
+        <el-radio label="2">{{loc('woman')}}</el-radio>
       </el-radio-group>
     </el-form-item>
 
@@ -58,13 +56,16 @@
 
       const confirm = async () => {
         // form.validate();
-        console.log('save:', person.value);
+        // console.log('save:', person.value);
         const result = await store.postData('persons', person.value);
-        console.log(result);
+        if (!('data' in result && 'id' in result.data)) {
+          console.log("error", result);
+        }
+
         router.push('/persons');
       };
 
-      return { confirm, person };
+      return { confirm, person,  loc: store.loc };
     },
     components: {
       MainTitle,

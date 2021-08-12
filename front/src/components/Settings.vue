8b4c89ad58db693b093a65e99ed29d9ecb7827d0
yaskevich <alyaxey.yaskevich@gmail.com>
2021-08-12 23:33:55 +0300

    localize Settings [front]

--- front/src/components/Settings.vue
+++ front/src/components/Settings.vue
@@ -1,73 +1,72 @@
 <template>
-    <MainTitle title="Проект" :callback="confirm" text="Сохранить"></MainTitle>
-    <el-form label-width="220px" :model="settings" ref="formRef" :rules="rules">
 
-      <el-form-item label="Основная персона">
-      <el-select
-          v-model="settings.persona"
-          filterable
-          placeholder="Персоны"
-          >
-          <el-option
-            v-for="item in persons"
-            :key="item.id"
-            :label="item.value"
-            :value="item.id">
-          </el-option>
-        </el-select>
-      </el-form-item>
+  <MainTitle :title="loc('settings')" :callback="confirm" :text="loc('save')"></MainTitle>
+  <el-form label-width="220px" :model="settings" ref="formRef" :rules="rules">
 
-      <el-form-item prop="title" label="Название проекта">
-        <el-input placeholder="..." v-model="settings.title" class="text-input"></el-input>
-      </el-form-item>
+    <el-form-item :label="loc('mainperson')">
+      <el-select v-model="settings.persona" filterable :placeholder="loc('persons')">
+        <el-option v-for="item in persons"
+                   :key="item.id"
+                   :label="item.value"
+                   :value="item.id">
+        </el-option>
+      </el-select>
+    </el-form-item>
 
-    </el-form>
+    <el-form-item prop="title" :label="loc('projtitle')">
+      <el-input placeholder="..." v-model="settings.title" class="text-input"></el-input>
+    </el-form-item>
+
+  </el-form>
+
 </template>
 
 <script lang="ts">
-import { onBeforeMount, ref, reactive } from 'vue';
-import store from "../store";
-import MainTitle from './MainTitle.vue';
 
-export default {
-  name: "Settings",
+  import { onBeforeMount, ref, reactive } from 'vue';
+  import store from '../store';
+  import MainTitle from './MainTitle.vue';
 
-  setup() {
-    const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
-    let persons = ref([]);
-    const settings = reactive({});
+  export default {
+    name: 'Settings',
 
-    onBeforeMount(async() => {
-      const personsData = await store.getData("persons");
-      persons.value = personsData.data.map (x => ({...x, value: x.firstname + ' ' + x.lastname}) );
-      console.log(persons);
-      Object.assign(settings, store.state.user.settings);
-    });
+    setup() {
+      const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
+      let persons = ref([]);
+      const settings = reactive({});
 
-    const confirm = () => {
-      formRef.value?.validate(async(valid) => {
-        if(valid) {
-          console.log('save:', settings.value);
-          Object.assign(store.state.user.settings, settings);
-          const result = await store.postData("settings", settings);
-          console.log(result);
-        } else{
-          console.log("form not valid");
-          return false;
-        }
+      onBeforeMount(async () => {
+        const personsData = await store.getData('persons');
+        persons.value = personsData.data.map(x => ({ ...x, value: x.firstname + ' ' + x.lastname }));
+        // console.log(persons);
+        Object.assign(settings, store.state.user.settings);
       });
-    };
 
-    const  rules = {
-      title: [
-        { required: true, message: 'Поле должно быть заполнено', trigger: 'blur' }
-      ],
-    };
+      const confirm = () => {
+        formRef.value?.validate(async valid => {
+          if (valid) {
+            // console.log('save:', settings.value);
+            Object.assign(store.state.user.settings, settings);
+            const result = await store.postData('settings', settings);
+            if (!('data' in result && 'id' in result.data)) {
+              console.log("error", result);
+            }
+          } else {
+            console.log('form not valid');
+            return false;
+          }
+        });
+      };
 
-    return { confirm, settings, persons, rules, formRef, };
-  },
-  components: {
-    MainTitle,
-  }
-};
+      const rules = {
+        title: [{ required: true, message: store.loc('fieldnonempty'), trigger: 'blur' }],
+      };
+
+      return { confirm, settings, persons, rules, formRef,  loc: store.loc };
+    },
+    components: {
+      MainTitle,
+    },
+  };
+
 </script>

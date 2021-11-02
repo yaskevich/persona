a5ed4032112da01b615f4778148eb0b3faeb8ff2
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-03 00:00:29 +0300

    remove Passport session initialization [back]

--- back/index.js
+++ back/index.js
@@ -49,7 +49,7 @@ if (!process.env.JWT_SECRET){
 	app.use(compression());
 	app.set('trust proxy', 1);
 	app.use(passport.initialize());
-	app.use(passport.session());
+	// app.use(passport.session());
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(history());

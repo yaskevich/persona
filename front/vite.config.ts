b37aa453b93e360b76c71c9dcdaae6e4d846944a
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-01-13 21:56:37 +0100

    upgrade Vite config [front]

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,18 +1,43 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from 'vite';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
   server: {
-    port: 3080,
+    port: Number(loadEnv('', process.cwd(), 'VITE_PORT')?.['VITE_PORT']) || 8080,
     proxy: {
-     '/api': {
-       target: 'http://localhost:3081',
-       changeOrigin: true,
-       secure: false,
-       ws: true,
-     }
-   }
-  }
-})
+      '/api': {
+        target: 'http://localhost:' + loadEnv('', process.cwd(), 'VITE_PROXY')?.['VITE_PROXY'] || String(8081),
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+});

c3dca756a2ca22442d62f8ca589bac2e8a142d58
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-01-23 18:34:51 +0100

    update framework setup [front]

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,14 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
-import locale from 'element-plus/lib/locale/lang/ru'
-import 'dayjs/locale/ru'
-import router from './router'
-import { createMetaManager } from "vue-meta"
+import { createApp } from 'vue';
+import App from './App.vue';
+import ElementPlus from 'element-plus';
+// import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/dist/index.css';
+import locale from 'element-plus/dist/locale/ru.mjs';
+import 'dayjs/locale/ru';
+import router from './router';
+import { createMetaManager } from 'vue-meta';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
-createApp(App)
-  .use(createMetaManager())
-  .use(router)
-  .use(ElementPlus, { locale })
-  .mount('#app');
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(`el-icon-${key.toLowerCase()}`, component);
+}
+
+app.use(createMetaManager()).use(router).use(ElementPlus, { locale }).mount('#app');

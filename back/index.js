833cb5e7fcc4982643fc71e57a6ebae0b4abe160
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-02-01 11:38:23 +0100

    add user/reset post route [back]

--- back/index.js
+++ back/index.js
@@ -1,6 +1,3 @@
-'use strict';
-
-import path from 'path';
 import fs from 'fs';
 import express from 'express';
 import compression from 'compression';
@@ -8,109 +5,113 @@ import bodyParser from 'body-parser';
 import passport from 'passport';
 import passportJWT from 'passport-jwt';
 import jwt from 'jsonwebtoken';
-import db from './db.js';
 import history from 'connect-history-api-fallback';
+// import { fileURLToPath } from 'url';
+import db from './db.js';
 
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-if (!process.env.JWT_SECRET){
-	console.error('Error: secret key should be provided as an environment variable!');
-	process.exit(1);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+if (!process.env.JWT_SECRET) {
+  console.error('Error: secret key should be provided as an environment variable!');
+  process.exit(1);
 }
 const __package = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 (async () => {
-	const app = express();
-	const port = process.env.PORT || 8080;
-	const JWTStrategy   = passportJWT.Strategy;
-	const ExtractJWT = passportJWT.ExtractJwt;
+  const app = express();
+  const port = process.env.PORT || 8080;
+  const JWTStrategy = passportJWT.Strategy;
+  const ExtractJWT = passportJWT.ExtractJwt;
 
-	const createToken = (user) => {
-	  return jwt.sign({
-	    iss: 'persona',
-	    sub: user.id,
-	    iat: new Date().getTime(),
-	    exp: new Date().setDate(new Date().getDate() + 1)
-	  }, process.env.JWT_SECRET);
-	};
+  const createToken = (user) => jwt.sign({
+    iss: 'persona',
+    sub: user.id,
+    iat: new Date().getTime(),
+    exp: new Date().setDate(new Date().getDate() + 1)
+  }, process.env.JWT_SECRET);
 
-	const strategy  = new JWTStrategy(
-		{
-			jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-			secretOrKey   : process.env.JWT_SECRET
-		},
-		(jwtPayload, done) =>
-			db.getUserDataByID(jwtPayload.sub)
-				.then(user => { return done(null, user); })
-				.catch(err => { return done(err); })
-		);
+  const strategy = new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET
+    },
+    (jwtPayload, done) => db.getUserDataByID(jwtPayload.sub)
+      .then((user) => done(null, user))
+      .catch((err) => done(err))
+  );
 
-	passport.use(strategy);
-	const auth = passport.authenticate('jwt', {session: false});
-	app.use(compression());
-	app.set('trust proxy', 1);
-	app.use(passport.initialize());
-	// app.use(passport.session());
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(history());
-	app.use(express.static('public'));
+  passport.use(strategy);
+  const auth = passport.authenticate('jwt', { session: false });
+  app.use(compression());
+  app.set('trust proxy', 1);
+  app.use(passport.initialize());
+  // app.use(passport.session());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(history());
+  app.use(express.static('public'));
 
-	app.post('/api/user/login', async(req, res) => {
-		const userData = await db.getUserData(req.body['email'], req.body['password']);
-		if (userData && Object.keys(userData).length && !userData.hasOwnProperty('error') ) {
-			console.log(req.body['email'], '<SUCCESS>');
-			const token = createToken(userData);
-			userData['token'] = token;
-			res.json(userData);
-		} else {
-			console.log(`login attempt as [${req.body['email']}]•[${req.body['password']}]►${userData.error}◄`);
-			res.json(userData);
-		}
-	});
+  app.post('/api/user/login', async (req, res) => {
+    const userData = await db.getUserData(req.body.email, req.body.password);
+    if (userData && Object.keys(userData).length && !userData?.error) {
+      console.log(req.body.email, '<SUCCESS>');
+      const token = createToken(userData);
+      userData.token = token;
+      res.json(userData);
+    } else {
+      console.log(`login attempt as [${req.body.email}]•[${req.body.password}]►${userData.error}◄`);
+      res.json(userData);
+    }
+  });
 
-	app.get('/api/user/logout', auth, async(req, res) => {
-		console.log('logging out');
-		// You can add 'issue time' to token and maintain 'last logout time' for each user on the server.
-		// When you check token validity, also check 'issue time' be after 'last logout time'.
-		// res.redirect('/login');
-	});
+  //   app.get('/api/user/logout', auth, async (req, res) => {
+  // console.log('logging out');
+  // You can add 'issue time' to token and maintain 'last logout time' for each user on the server.
+  // When you check token validity, also check 'issue time' be after 'last logout time'.
+  // res.redirect('/login');
+  //   });
 
-	app.get('/api/user/info', auth, async(req,res) => {
-		const settings = await db.getData("settings", 1);
-		const stats = await db.getStats();
-		res.json(Object.assign(req.user, {"settings": settings?.[0], "stats": stats, "commit": process.env.COMMIT, "server": __package.version, }));
-	 });
+  app.get('/api/user/info', auth, async (req, res) => {
+    const settings = await db.getData('settings', 1);
+    const stats = await db.getStats();
+    res.json(Object.assign(req.user, {
+      settings: settings?.[0], stats, commit: process.env.COMMIT, server: __package.version,
+    }));
+  });
 
-	app.post('/api/user/reg', async(req,res) => {
-		const result = await db.createUser(Object.assign({"privs": 5}, req.body), false);
-		res.json(result);
-	});
+  app.post('/api/user/reg', async (req, res) => {
+    const result = await db.createUser({ privs: 5, ...req.body }, false);
+    res.json(result);
+  });
 
-	app.post('/api/user/add', auth, async(req,res) => {
-		const result = await db.createUser(req.body, true);
-		res.json(result);
-	});
+  app.post('/api/user/add', auth, async (req, res) => {
+    const result = await db.createUser(req.body, true);
+    res.json(result);
+  });
 
-	app.post('/api/:table', auth, async(req,res) => {
-		console.log('POST params', req.params, 'query', req.query);
-		if (req.params['table'] === "users" && (!(req.user.id === req.body.id || req.user.privs == 1))){
-			return res.json({"error": "access denied"});
-		}
-		res.json(await db.setData(req.body, req.params['table'], req.user));
-	 });
+  app.post('/api/user/reset', auth, async (req, res) => {
+    const result = await db.resetPassword(req.user, req.body.id);
+    res.json(result);
+  });
 
-	app.delete('/api/:table/:id', auth, async(req,res) => {
-		console.log('DELETE params', req.params, 'query', req.query);
-		res.json(await db.deleteData(req.params['table'], req.params['id'], req.user));
-	});
+  app.post('/api/:table', auth, async (req, res) => {
+    console.log('POST params', req.params, 'query', req.query);
+    if (req.params.table === 'users' && (!(req.user.id === req.body.id || req.user.privs === 1))) {
+      return res.json({ error: 'access denied' });
+    }
+    return res.json(await db.setData(req.body, req.params.table, req.user));
+  });
 
-	app.get('/api/:table', auth, async(req,res) => {
-	 console.log('GET params', req.params, 'query', req.query);
-	 res.json(await db.getData(req.params['table'], req.query['id'], req.query['off'], req.query['lim'],));
- });
+  app.delete('/api/:table/:id', auth, async (req, res) => {
+    console.log('DELETE params', req.params, 'query', req.query);
+    res.json(await db.deleteData(req.params.table, req.params.id, req.user));
+  });
 
-	app.listen(port);
-	// console.log(`Running at port ${port}`);
-})()
+  app.get('/api/:table', auth, async (req, res) => {
+    console.log('GET params', req.params, 'query', req.query);
+    res.json(await db.getData(req.params.table, req.query.id, req.query.off, req.query.lim,));
+  });
+
+  app.listen(port);
+  console.log(`Running at port ${port}`);
+})();

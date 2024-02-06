baa57d4126ccae08948c0d7fe9d85eee372f8ec4
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-02-06 18:06:53 +0100

    rewrite in setup API [front]

--- front/src/components/MainTitle.vue
+++ front/src/components/MainTitle.vue
@@ -1,33 +1,25 @@
 <template>
-
   <el-row type="flex" justify="center">
-    <el-space size="medium" :spacer="spacer">
-      <h3>{{title}}</h3>
-      <el-button v-if="callback" size="medium" type="info" plain @click="callback">{{text||loc('add')}}</el-button>
+    <el-space size="default" :spacer="spacer">
+      <h3>{{ title }}</h3>
+      <el-button v-if="callback" size="default" type="info" plain @click="() => callback">{{ text || store.loc('add') }}</el-button>
     </el-space>
   </el-row>
-
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 
-  import { ElDivider } from 'element-plus';
-  import { defineComponent, h } from 'vue';
-  import store from '../store';
+import { ElDivider } from 'element-plus';
+import { h } from 'vue';
+import store from '../store';
 
-  export default defineComponent({
-    props: {
-      title: String,
-      callback: Function,
-      text: String,
-    },
+defineProps<{
+  title: string,
+  callback: Function,
+  text?: string,
+}>();
 
-    setup() {
-      return {
-        spacer: h(ElDivider, { direction: 'vertical' }),
-        loc: store.loc,
-      };
-    },
-  });
+
+const spacer = h(ElDivider, { direction: 'vertical' });
 
 </script>

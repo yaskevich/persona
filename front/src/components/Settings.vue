b7eaf881be0207f61b5e9c96599bb051b215e7e1
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-02-11 16:51:45 +0100

    rewrite in setup API [front]

--- front/src/components/Settings.vue
+++ front/src/components/Settings.vue
@@ -1,14 +1,10 @@
 <template>
-
   <MainTitle :title="loc('settings')" :callback="confirm" :text="loc('save')"></MainTitle>
-  <el-form label-width="220px" :model="settings" ref="formRef" :rules="rules">
+  <el-form label-width="220px" :model="settings" ref="formRef" :rules="rules" v-if="isLoaded">
 
     <el-form-item :label="loc('mainperson')">
       <el-select v-model="settings.mainperson" filterable :placeholder="loc('persons')">
-        <el-option v-for="item in persons"
-                   :key="item.id"
-                   :label="item.value"
-                   :value="item.id">
+        <el-option v-for="item in persons" :key="item.id" :label="store.getLabel(item)" :value="item.id">
         </el-option>
       </el-select>
     </el-form-item>
@@ -19,64 +15,56 @@
 
     <el-form-item prop="lang" :label="loc('lang')">
       <el-select v-model="settings.lang" :placeholder="loc('lang')">
-        <el-option v-for="(item, key) in langs"
-                   :key="key"
-                   :label="item"
-                   :value="key">
+        <el-option v-for="(item, key) in langs" :key="key" :label="item" :value="key">
         </el-option>
       </el-select>
     </el-form-item>
 
   </el-form>
-
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 
-  import { onBeforeMount, ref, reactive } from 'vue';
-  import store from '../store';
-  import MainTitle from './MainTitle.vue';
+import { onBeforeMount, ref, reactive } from 'vue';
+import type { FormInstance, FormRules } from 'element-plus';
+import store from '../store';
+import MainTitle from './MainTitle.vue';
 
-  export default {
-    name: 'Settings',
+const formRef = ref<FormInstance>();
+let persons = ref([] as Array<IPerson>);
+const settings = reactive({} as ISettings);
+const isLoaded = ref(false);
 
-    setup() {
-      const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
-      let persons = ref([]);
-      const settings = reactive({});
+onBeforeMount(async () => {
+  const personsData = await store.getData('persons');
+  persons.value = personsData.data as Array<IPerson>;
+  // console.log(persons);
+  Object.assign(settings, store?.state?.user?.settings);
+  isLoaded.value = true;
+});
 
-      onBeforeMount(async () => {
-        const personsData = await store.getData('persons');
-        persons.value = personsData.data.map(x => ({ ...x, value: x.firstname + ' ' + x.lastname }));
-        // console.log(persons);
-        Object.assign(settings, store.state.user.settings);
-      });
+const confirm = () => {
+  formRef.value?.validate(async (valid: boolean) => {
+    if (valid) {
+      // console.log('save:', settings.value);
+      if (store?.state?.user)
+        Object.assign(store.state.user.settings, settings);
+      const result = await store.postData('settings', settings);
+      if (!('data' in result && 'id' in result.data)) {
+        console.log('error', result);
+      }
+    } else {
+      console.log('form not valid');
+      return false;
+    }
+  });
+};
 
-      const confirm = () => {
-        formRef.value?.validate(async valid => {
-          if (valid) {
-            // console.log('save:', settings.value);
-            Object.assign(store.state.user.settings, settings);
-            const result = await store.postData('settings', settings);
-            if (!('data' in result && 'id' in result.data)) {
-              console.log('error', result);
-            }
-          } else {
-            console.log('form not valid');
-            return false;
-          }
-        });
-      };
+const rules = {
+  title: [{ required: true, message: store.loc('fieldnonempty'), trigger: 'blur' }],
+};
 
-      const rules = {
-        title: [{ required: true, message: store.loc('fieldnonempty'), trigger: 'blur' }],
-      };
-
-      return { confirm, settings, persons, rules, formRef, loc: store.loc, langs: store.langs };
-    },
-    components: {
-      MainTitle,
-    },
-  };
+const loc = store.loc;
+const langs = store.langs;
 
 </script>

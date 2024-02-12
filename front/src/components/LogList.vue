1556a8944cd0bcfae232fe8ac1375be0af82312b
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-02-12 14:40:14 +0100

    rewrite in setup API [front]

--- front/src/components/LogList.vue
+++ front/src/components/LogList.vue
@@ -1,30 +1,29 @@
 <template>
-
   <el-row type="flex" justify="center">
     <h3>
-      {{loc('userlog')}}
+      {{ loc('userlog') }}
     </h3>
   </el-row>
 
   <el-row v-for="(value, key) in datum.logs" :gutter="20" :key="key">
     <el-col :span="6">
       <div class="grid-content bg-purple-light">
-        {{datum.users[value.user_id]["firstname"] + ' ' + datum.users[value.user_id]["lastname"]}}
+        {{ store.getLabel(datum.users[value.user_id]) }}
       </div>
     </el-col>
     <el-col :span="6">
       <div class="grid-content bg-purple">
-        {{store.dateToString(value.created)}}
+        {{ store.dateToString(value.created) }}
       </div>
     </el-col>
     <el-col :span="4">
       <div class="grid-content bg-purple">
-        {{loc(value.table_name)}}
+        {{ loc(value.table_name) }}
       </div>
     </el-col>
     <el-col :span="4">
       <div class="grid-content bg-purple">
-        {{store.classify(value)}}
+        {{ store.classify(value) }}
       </div>
     </el-col>
     <el-col :span="4">
@@ -37,41 +36,31 @@
   </el-row>
 
   <el-row type="flex" justify="center">
-    <el-pagination layout="prev, pager, next"
-                   :total="total"
-                   :page-size="size"
-                   :default-page-size="size"
-                   :default-current-page="1"
-                   @update:current-page="updatePagination">
+    <el-pagination layout="prev, pager, next" :total="total" :page-size="size" :default-page-size="size"
+      :default-current-page="1" @update:current-page="updatePagination">
     </el-pagination>
   </el-row>
-
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 
-  import { defineComponent, reactive, onBeforeMount } from 'vue';
-  import store from '../store';
+import { reactive, onBeforeMount } from 'vue';
+import store from '../store';
 
-  export default defineComponent({
-    setup() {
-      const datum = reactive({ users: [], logs: [] });
-      const total = store.state?.user?.stats?.logs;
-      const size = 30;
+const datum = reactive({ users: [], logs: [] as Array<ILog> });
+const total = store.state?.user?.stats?.logs;
+const size = 30;
+const loc = store.loc;
 
-      onBeforeMount(async () => {
-        await store.getUser(); // update stats
-        await store.getDataMulti(datum, { users: 'id' }, {}, { logs: [0, size] });
-      });
+onBeforeMount(async () => {
+  await store.getUser(); // update stats
+  await store.getDataMulti(datum, { users: 'id' }, {}, { logs: [0, size] });
+});
 
-      const updatePagination = async e => {
-        // console.log('page', e);
-        const { data } = await store.getData('logs', undefined, [(e - 1) * size, size]);
-        datum.logs = data;
-      };
-
-      return { datum, store, loc: store.loc, total, size, updatePagination };
-    },
-  });
+const updatePagination = async (e: number) => {
+  // console.log('page', e);
+  const { data } = await store.getData('logs', undefined, [(e - 1) * size, size]);
+  datum.logs = data;
+};
 
 </script>

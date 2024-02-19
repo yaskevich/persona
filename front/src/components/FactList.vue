509b25e3d0824bd10ad6d0768164cb0df0b1970e
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-02-19 17:10:56 +0100

    rewrite in setup API [front]

--- front/src/components/FactList.vue
+++ front/src/components/FactList.vue
@@ -1,14 +1,8 @@
 <template>
-
   <MainTitle :title="loc('facts')" :callback="confirm"></MainTitle>
   <el-row type="flex" justify="center">
-    <el-switch style="display: block;"
-               v-model="sortMode"
-               active-color="black"
-               inactive-color="gray"
-               :active-text="loc('bydate')"
-               :inactive-text="loc('byts')"
-               @change="changeSort">
+    <el-switch style="display: block;" v-model="sortMode" active-color="black" inactive-color="gray"
+      :active-text="loc('bydate')" :inactive-text="loc('byts')" @change="changeSort">
     </el-switch>
   </el-row>
 
@@ -16,104 +10,88 @@
 
     <el-col :span="4">
       <div class="grid-content bg-purple-light">
-        <span :title="value.stamp">{{value.datedesc}}</span>
+        <span :title="String(value.stamp)">{{ value.datedesc }}</span>
       </div>
     </el-col>
 
     <el-col :span="16">
       <div class="grid-content bg-purple">
-        {{value.title}}
+        {{ value.title }}
       </div>
     </el-col>
 
     <el-col :span="4">
       <div class="grid-content bg-purple">
         <router-link :to="'/fact/' + value.id">
-          <el-button type="text" size="mini" icon="el-icon-edit" plain class="full-width"></el-button>
+          <el-button type="" link size="small" icon="el-icon-edit" plain class="full-width"></el-button>
         </router-link>
       </div>
     </el-col>
 
   </el-row>
-
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 
-  import { defineComponent, ref, reactive, onBeforeMount, ComponentPublicInstance, h } from 'vue';
-  import { ElForm } from 'element-plus';
-  import router from '../router';
-  import store from '../store';
-  import MainTitle from './MainTitle.vue';
+import { ref, onBeforeMount } from 'vue';
+// import { ElForm } from 'element-plus';
+import router from '../router';
+import store from '../store';
+import MainTitle from './MainTitle.vue';
 
-  export default defineComponent({
-    setup() {
-      const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
-      const sortMode = ref(false);
-      const facts = ref([]);
+const sortMode = ref(false);
+const facts = ref([] as Array<IFact>);
 
-      const sortBy = (attr: String) => {
-        return attr === 'id'
-          ? (a, b) => {
-              return b[attr] - a[attr];
-            }
-          : (a, b) => {
-              return new Date(b[attr]) - new Date(a[attr]);
-            };
-      };
+const sortBy = (attr: string) => {
+  return attr === 'id'
+    ? (a: keyable, b: keyable) => {
+      return b[attr] - a[attr];
+    }
+    : (a: keyable, b: keyable) => {
+      return new Date(b[attr]) > new Date(a[attr]);
+    };
+};
 
-      const loadFacts = async () => {
-        const attr = sortMode.value ? 'stamp' : 'id';
-        const result = await store.getData('facts');
-        // console.log(result);
-        if ('data' in result && !('error' in result.data)) {
-          if (Object.keys(result.data).length) {
-            facts.value = result.data.sort(sortBy(attr));
-          } else {
-            console.log('table is empty');
-          }
-        }
-      };
+const loadFacts = async () => {
+  const attr = sortMode.value ? 'stamp' : 'id';
+  const result = await store.getData('facts');
+  // console.log(result);
+  if ('data' in result && !('error' in result.data)) {
+    if (Object.keys(result.data).length) {
+      facts.value = result.data.sort(sortBy(attr));
+    } else {
+      console.log('table is empty');
+    }
+  }
+};
 
-      const changeSort = async () => {
-        if (store.state.user.prefs) {
-          if (store.state.user.prefs.sorts) {
-            store.state.user.prefs.sorts['facts'] = sortMode.value;
-          } else {
-            store.state.user.prefs = { sorts: { facts: sortMode.value } };
-          }
-        } else {
-          store.state.user.prefs = { sorts: { facts: sortMode.value } };
-        }
-        const userData = { id: store.state.user.id, prefs: store.state.user.prefs };
-        const result = await store.postData('users', userData);
-        // console.log('save prefs', result);
-        await loadFacts();
-      };
+const changeSort = async () => {
+  if (store?.state?.user) {
+    if (store?.state?.user?.prefs) {
+      if (store.state.user.prefs.sorts) {
+        store.state.user.prefs.sorts['facts'] = sortMode.value;
+      } else {
+        store.state.user.prefs = { sorts: { facts: sortMode.value } };
+      }
+    } else {
+      store.state.user['prefs'] = { sorts: { facts: sortMode.value } };
+    }
+    const userData = { id: store?.state?.user?.id, prefs: store?.state?.user?.prefs };
+    const result = await store.postData('users', userData);
+    // console.log('save prefs', result);
+    await loadFacts();
+  }
+};
 
-      onBeforeMount(async () => {
-        sortMode.value = Boolean(store.state.user?.prefs?.sorts?.['facts']);
-        await loadFacts();
-      });
+onBeforeMount(async () => {
+  sortMode.value = Boolean(store.state.user?.prefs?.sorts?.['facts']);
+  await loadFacts();
+});
 
-      const confirm = () => {
-        router.push('/fact');
-      };
+const confirm = () => {
+  router.push('/fact');
+};
 
-      return {
-        confirm,
-        formRef,
-        facts,
-        sortMode,
-        loadFacts,
-        changeSort,
-        loc: store.loc,
-      };
-    },
-
-    components: {
-      MainTitle,
-    },
-  });
+const loc = store.loc;
 
 </script>

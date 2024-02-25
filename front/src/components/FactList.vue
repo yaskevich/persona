06b908a947decf91dc5b3ec356ed128b3c576023
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-02-25 14:33:08 +0100

    fix sorting [front]

--- front/src/components/FactList.vue
+++ front/src/components/FactList.vue
@@ -1,32 +1,32 @@
 <template>
-  <MainTitle :title="loc('facts')" :callback="confirm"></MainTitle>
+  <MainTitle :title="store.loc('facts')" :callback="confirm"></MainTitle>
   <el-row type="flex" justify="center">
     <el-switch style="display: block;" v-model="sortMode" active-color="black" inactive-color="gray"
-      :active-text="loc('bydate')" :inactive-text="loc('byts')" @change="changeSort">
+      :active-text="store.loc('bydate')" :inactive-text="store.loc('byts')" @change="changeSort">
     </el-switch>
   </el-row>
 
   <el-row v-for="(value, key) in facts" :gutter="20" :key="key">
 
-    <el-col :span="4">
-      <div class="grid-content bg-purple-light">
-        <span :title="String(value.stamp)">{{ value.datedesc }}</span>
-      </div>
+    <el-col :span="8">
+      <el-text :title="String(value.stamp)">
+        {{ value.datedesc }}
+      </el-text>
     </el-col>
 
     <el-col :span="16">
-      <div class="grid-content bg-purple">
-        {{ value.title }}
-      </div>
+      <router-link :to="'/fact/' + value.id">
+        <el-text truncated>
+          {{ value.title }}
+        </el-text>
+      </router-link>
+
     </el-col>
 
-    <el-col :span="4">
+    <!-- <el-col :span="4">
       <div class="grid-content bg-purple">
-        <router-link :to="'/fact/' + value.id">
-          <el-button type="" link size="small" icon="el-icon-edit" plain class="full-width"></el-button>
-        </router-link>
       </div>
-    </el-col>
+    </el-col> -->
 
   </el-row>
 </template>
@@ -42,23 +42,17 @@ import MainTitle from './MainTitle.vue';
 const sortMode = ref(false);
 const facts = ref([] as Array<IFact>);
 
-const sortBy = (attr: string) => {
-  return attr === 'id'
-    ? (a: keyable, b: keyable) => {
-      return b[attr] - a[attr];
-    }
-    : (a: keyable, b: keyable) => {
-      return new Date(b[attr]) > new Date(a[attr]);
-    };
-};
+const sortBy = (attr: string, order = 1) => (a: keyable, b: keyable) => a[attr] === b[attr] ? 0 : (Number(a[attr] < b[attr]) || -1) * order;
 
 const loadFacts = async () => {
   const attr = sortMode.value ? 'stamp' : 'id';
+
   const result = await store.getData('facts');
-  // console.log(result);
   if ('data' in result && !('error' in result.data)) {
     if (Object.keys(result.data).length) {
-      facts.value = result.data.sort(sortBy(attr));
+      const sorted = result.data.sort(sortBy(attr));
+      // console.log(attr, sorted);
+      facts.value = sorted;
     } else {
       console.log('table is empty');
     }
@@ -91,7 +85,5 @@ onBeforeMount(async () => {
 const confirm = () => {
   router.push('/fact');
 };
-
-const loc = store.loc;
 
 </script>

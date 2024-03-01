a9f1abd096791f9ce59d81a67cd125ef37411de1
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-03-01 16:03:59 +0100

    handle editing person note [front]

--- front/src/components/PersonList.vue
+++ front/src/components/PersonList.vue
@@ -21,6 +21,12 @@
           style="max-width:110px;"></el-input>
       </el-form-item>
 
+      <el-form-item prop="note">
+        <el-input :placeholder="loc('note')" prop="note" v-model="form.note" class="text-input"
+          style="max-width:110px;"></el-input>
+      </el-form-item>
+
+
       <el-form-item>
         <el-radio-group v-model="form.sex">
           <el-radio :label="1">{{ loc('man') }}</el-radio>
@@ -38,43 +44,14 @@
     </el-row>
 
     <el-row v-for="(value, key) in filtered()" :gutter="20" :key="key">
-
-      <el-col :span="4">
-        <div class="grid-content bg-purple"><i :class="value.sex === 1 ? 'el-icon-male' : 'el-icon-female'"></i>
-          {{ value.firstname }}</div>
-      </el-col>
-
-      <el-col :span="4">
-        <div class="grid-content bg-purple-light">{{ value.patroname }}</div>
-      </el-col>
-
-      <el-col :span="4">
-        <div class="grid-content bg-purple">{{ value.lastname }}</div>
-      </el-col>
-
-      <el-col :span="4">
-        <div class="grid-content bg-purple-light">{{ value.wikidata || '&nbsp;' }}</div>
-      </el-col>
-
-      <el-col :span="4">
-        <div class="grid-content bg-purple">
-          <router-link :to="'/person/' + value.id">
-            <el-button type="" link size="small" icon="el-icon-edit" plain class="full-width"></el-button>
-          </router-link>
-        </div>
-      </el-col>
-
-      <el-col :span="4">
-        <div class="grid-content bg-purple-light">
-          <!-- <el-button type="primary" @click="confirm">{{loc('remove')}}</el-button> -->
-          <el-popconfirm :title="loc('confirmdel')" :confirmButtonText="loc('yes')" :cancelButtonText="loc('no')"
-            @confirm="deletePerson(value.id, key)">
-            <template #reference>
-              <el-button>{{ loc('remove') }}</el-button>
-            </template>
-          </el-popconfirm>
-        </div>
-      </el-col>
+      <!-- <el-tooltip class="box-item" effect="dark" :content="value.firstname + ' ' + value.patroname" placement="top-start"> -->
+      <el-space>
+        <el-button plain :icon="value.sex === 1 ? 'el-icon-male' : 'el-icon-female'"
+          @click="$router.push('/person/' + value.id)">{{ value.firstname }}
+          {{ value.patroname }} {{ value.lastname }}</el-button>
+        <!-- </el-tooltip> -->
+        <el-tag v-if="value?.note" effect="plain" size="large">{{ value.note }}</el-tag>
+      </el-space>
 
     </el-row>
   </div>
@@ -89,7 +66,7 @@ import MainTitle from './MainTitle.vue';
 
 const isLoaded = ref(false);
 const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
-const form = reactive({ firstname: '', lastname: '', patroname: '', sex: 1, wikidata: '' } as IPerson);
+const form = reactive({ firstname: '', lastname: '', patroname: '', sex: 1, wikidata: '', note: '' } as IPerson);
 const persons = reactive([] as Array<IPerson>);
 const filterString = ref('');
 
@@ -101,13 +78,13 @@ onBeforeMount(async () => {
   isLoaded.value = true;
 });
 
-const deletePerson = async (id: number, key: number) => {
-  const { data } = await store.deleteById('persons', id);
-  if (data && 'id' in data) {
-    // console.log('deleted', id);
-    persons.splice(key, 1);
-  }
-};
+// const deletePerson = async (id: number, key: number) => {
+//   const { data } = await store.deleteById('persons', id);
+//   if (data && 'id' in data) {
+//     // console.log('deleted', id);
+//     persons.splice(key, 1);
+//   }
+// };
 
 const resetForm = () => {
   formRef.value?.resetFields();
@@ -119,7 +96,7 @@ const confirm = () => {
       const { data } = await store.postData('persons', form);
       // console.log(result);
       if (data && 'id' in data) {
-        persons.unshift({ ...form } as IPerson);
+        persons.unshift({ ...form, id: data.id } as IPerson);
         // console.log('success', persons);
         formRef.value?.resetFields();
       }
@@ -149,7 +126,7 @@ const filtered = () => {
   const re = new RegExp(filterString.value, 'i');
   return filterString.value
     ? persons.filter(
-      x => x.firstname.search(re) !== -1 || x.lastname.search(re) !== -1 || x.patroname.search(re) !== -1
+      x => x?.firstname && x.firstname.search(re) !== -1 || x?.lastname && x.lastname.search(re) !== -1 || x?.patroname && x.patroname.search(re) !== -1 || x?.note && x.note.search(re) !== -1
     )
     : persons;
 };

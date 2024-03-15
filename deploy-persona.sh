99275631ddcaa3a41489e50f4d1ceed378e7dcd9
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-03-15 22:25:03 +0100

    update deployment script

--- deploy-persona.sh
+++ deploy-persona.sh
@@ -1,10 +1,26 @@
-pm2 delete persona
-rm -rf persona
-git clone https://github.com/yaskevich/persona  --depth 1
-cd persona
+SCRIPT=$(basename "$0")
+[[ $SCRIPT =~ ([^-]*)-(.*)\.sh ]]
+NAME=${BASH_REMATCH[2]}
+# echo "${NAME}"
+
+if [[ -z "$NAME" ]]; then
+    echo 'Project name is empty. Quitting'
+    exit
+fi
+
+if [ ! -f $NAME.env ]; then
+    echo "ENV file '$NAME.env'was not found. Quitting..."
+    exit
+fi
+
+# echo "Project name is '$NAME'"
+
+pm2 delete $NAME
+rm -rf $NAME
+git clone https://github.com/yaskevich/persona $NAME --depth 1
+cd $NAME
 hash=`git rev-parse --short HEAD`
 # echo $hash
-rm -rf .git
 cd front
 npm install
 npm run build
@@ -15,10 +31,8 @@ rm ../* 2>/dev/null
 rm ../.* 2>/dev/null
 cd ../
 mv back/* .
-rm -rf front back
-cp ../persona.env .env
+rm -rf .git front back
+cp ../$NAME.env .env
 printf "\nCOMMIT=%s" $hash >> .env
 pm2 start ecosystem.config.cjs
 pm2 save
-
-

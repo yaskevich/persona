426ffe442ee1607555d5141a1a7e0e42fa589d9d
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-01-20 23:04:32 +0100

    process title and headings in udpipe function in nlp [back]

--- back/nlp.js
+++ back/nlp.js
@@ -10,8 +10,9 @@ const options = {
   textNodeName: 'text',
   trimValues: true,
   parseAttributeValue: true,
-  processEntities: true,
+  processEntities: false,
   preserveOrder: true,
+  // htmlEntities: true
 };
 
 const getNodeByName = (arr, name) => arr.find((x) => name in x)?.[name];
@@ -45,8 +46,9 @@ export default {
 
     const text = iconv.decode(buf, encoding);
     const parser = new XMLParser(options);
-    parser.addEntity('nbsp', ' ');
-    parser.addEntity('apos', '’');
+    // parser.addEntity('nbsp', ' ');
+    // parser.addEntity('apos', '’');
+    // parser.addEntity('quot', '"');
     const builder = new XMLBuilder(options);
     const jObj = parser.parse(text);
     // console.log(JSON.stringify(jObj));
@@ -59,59 +61,67 @@ export default {
     // return JSON.stringify(jObj.FictionBook.body.section);
   },
   parseEbook(buf) {
-    const unzipped = unzipSync(buf);
-    const q = ':@';
-    // console.log(unzipped);
-    // const content = Object.keys(unzipped)
-    //   .filter((filename) => unzipped[filename].length > 0)
-    //   .map((filename) => new File([unzipped[filename]], filename));
-    // console.log(content);
-    const meta = Buffer.from(unzipped['content.opf']).toString();
-    const parser = new XMLParser(options);
-    parser.addEntity('nbsp', ' ');
-    parser.addEntity('apos', '’');
-    const builder = new XMLBuilder(options);
-    const jObj = parser.parse(meta);
-    // console.log(JSON.stringify(jObj));
-    const nodeP = getNodeByName(jObj, 'package');
-    const nodeD = getNodeByName(nodeP, 'metadata');
-    const nodeM = getNodeByName(nodeP, 'manifest');
-    const title = getNodeByName(getNodeByName(nodeD, 'dc:title'), 'text');
-    const lang = getNodeByName(getNodeByName(nodeD, 'dc:language'), 'text');
-    const author = getNodeByName(getNodeByName(nodeD, 'dc:creator'), 'text');
+    try {
+      const unzipped = unzipSync(buf);
+      const q = ':@';
+      // console.log(unzipped);
+      // const content = Object.keys(unzipped)
+      //   .filter((filename) => unzipped[filename].length > 0)
+      //   .map((filename) => new File([unzipped[filename]], filename));
+      // console.log(content);
+      const meta = Buffer.from(unzipped['content.opf']).toString();
+      const parser = new XMLParser(options);
+      // parser.addEntity('nbsp', ' ');
+      // parser.addEntity('apos', '’');
+      // parser.addEntity('quot', '"');
+      const builder = new XMLBuilder(options);
+      const jObj = parser.parse(meta);
+      // console.log(JSON.stringify(jObj));
+      const nodeP = getNodeByName(jObj, 'package');
+      const nodeD = getNodeByName(nodeP, 'metadata');
+      const nodeM = getNodeByName(nodeP, 'manifest');
+      const title = getNodeByName(getNodeByName(nodeD, 'dc:title'), 'text');
+      const lang = getNodeByName(getNodeByName(nodeD, 'dc:language'), 'text');
+      const author = getNodeByName(getNodeByName(nodeD, 'dc:creator'), 'text');
 
-    const getData = (chunk) => {
-      const obj = parser.parse(chunk.toString())[1].html[1].body;
-      return obj.map((x) => (x?.div && x[q]?.class === 'POETRY' ? { blockquote: x.div, [q]: { class: 'poetry' } } : x));
-    };
+      const getData = (chunk) => {
+        const obj = parser.parse(chunk.toString())[1].html[1].body;
+        return obj.map((x) => (x?.div && x[q]?.class === 'POETRY' ? { blockquote: x.div, [q]: { class: 'poetry' } } : x));
+      };
 
-    const html = nodeM.filter((x) => x?.[':@']?.['media-type'] === 'application/xhtml+xml').map((x) => builder.build(getData(Buffer.from(unzipped[x?.[q]?.href])))).join('').replaceAll('<p> </p>', '')
-      .replaceAll('<div class="CLEAR"></div>', '');
-    // console.log(html);
-    return {
-      options: {
-        title, lang, author
-      },
-      html
-    };
+      const html = nodeM.filter((x) => x?.[':@']?.['media-type'] === 'application/xhtml+xml').map((x) => builder.build(getData(Buffer.from(unzipped[x?.[q]?.href])))).join('').replaceAll('<p> </p>', '')
+        .replaceAll('<div class="CLEAR"></div>', '')
+        .replaceAll('  ', ' ');
+      // console.log(html);
+      return {
+        options: {
+          title, lang, author
+        },
+        html
+      };
+    } catch (error) {
+      console.log('[ZIP]', error);
+      return {};
+    }
   },
-  async udpipe(content, langID) {
+  async udpipe(content, title, langID) {
     const body = new FormData();
     body.set('tokenizer', '');
     body.set('tagger', '');
     body.set('parser', '');
     body.set('model', langID);
-    body.set('data', content.replace(/<[^>]*>?/gm, ' ').replaceAll("'", '’'));
+    body.set('data', `${title}\n\n${content.replace(/(?<=<\/h\d>)/gm, '\n\n').replace(/<[^>]*>?/gm, ' ').replaceAll("'", '’')}`);
 
     const response = await fetch('https://lindat.mff.cuni.cz/services/udpipe/api/process', { method: 'POST', body });
 
-    if (!response.ok) {
-      console.log(response.status);
-      throw new Error(`Response status: ${response.status}`);
+    if (response.ok) {
+      const json = await response.json();
+      return json.result;
     }
 
-    const json = await response.json();
-    return json.result;
+    console.log(`Response status: ${response.status}`);
+    // throw new Error(`Response status: ${response.status}`);
+    return '';
   },
 
   conllToArray(data) {

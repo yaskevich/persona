83001a20aabc3c7537a7b0de14bebc9e83a71c23
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-02-06 23:41:58 +0100

    render CONLL data as table in Anno [front]

--- front/src/components/Anno.vue
+++ front/src/components/Anno.vue
@@ -1,7 +1,12 @@
 <template>
-    <h5>{{ work?.title }}</h5>
+    <!-- <h5>{{ arr?.title }}</h5> -->
     <!-- <el-button @click="saveText" type="warning">{{ store.loc('save') }}</el-button> -->
-    {{ work }}
+    <el-table :data="arr" border style="width: 100%" v-if="arr?.length">
+        <el-table-column prop="form" label="Word" width="180" />
+        <el-table-column prop="lemma" label="Lemma" width="180" />
+        <el-table-column prop="upos" label="PoS" :filters="tags.map((x: any) => ({ text: x, value: x }))"
+            :filter-method="filterTag" filter-placement="bottom-end" />
+    </el-table>
 </template>
 
 <script setup lang="ts">
@@ -12,17 +17,24 @@ import store from '../store';
 
 const vuerouter = useRoute();
 const work = ref({} as IWork);
+const arr = ref<Array<ICONLL>>();
 const html = ref('');
 const id = String(vuerouter.params.id);
+const tags = ref();
 
+
+const filterTag = (value: string, row: ICONLL) => {
+    return row.upos === value
+}
 
 onBeforeMount(async () => {
     // console.log('router id', id);
     if (id) {
         const { data } = await store.getData('annotation', id);
         if (data) {
-            work.value = data;
-            console.log('this work', work.value);
+            arr.value = data;
+            // console.log('this work', arr.value);
+            tags.value = [...new Set(data.map((x: any) => x.upos))]
         }
     }
 });

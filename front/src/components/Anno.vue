0ecfcc19ce617cf2bf10b099baeb2780fccdf8ab
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-02-09 23:40:17 +0100

    add virtualized table to Anno [front]

--- front/src/components/Anno.vue
+++ front/src/components/Anno.vue
@@ -1,8 +1,10 @@
 <template>
-    <!-- <h5>{{ arr?.title }}</h5> -->
+    <h5>{{ work?.title }}</h5>
     <!-- <el-button @click="saveText" type="warning">{{ store.loc('save') }}</el-button> -->
-    <el-table :data="arr" border style="width: 100%" v-if="arr?.length">
-        <el-table-column prop="form" :label="store.loc('word')" width="180" />
+    <el-auto-resizer>
+        <template #default="{ height, width }">
+            <el-table-v2 :data="arr" :columns="columns" :width="width" :height="height" fixed v-if="arr?.length">
+                <!-- <el-table-column prop="form" :label="store.loc('word')" width="180" />
         <el-table-column prop="lemma" :label="store.loc('lemma')" width="180" />
         <el-table-column prop="upos" label="PoS" :filters="tags.map((x: any) => ({ text: x, value: x }))" width="80"
             :filter-method="filterTag" filter-placement="bottom-end">
@@ -10,9 +12,9 @@
                 <el-tag type="info" :color="colors[tags.indexOf(scope.row.upos)]" disable-transitions>{{
                     scope.row.upos }}</el-tag>
             </template>
-        </el-table-column>
-        <el-table-column prop="feats" :label="store.loc('features')">
-            <template #default="scope">
+</el-table-column>
+<el-table-column prop="feats" :label="store.loc('features')">
+    <template #default="scope">
                 <el-space v-if="scope?.row?.feats && Object.keys(scope.row.feats)?.length">
                     <el-tooltip v-for="(val, key) in scope.row.feats" class="box-item" effect="dark"
                         :content="String(key)" placement="top-start">
@@ -20,13 +22,26 @@
                     </el-tooltip>
                 </el-space>
             </template>
-        </el-table-column>
-    </el-table>
+</el-table-column> -->
+            </el-table-v2>
+        </template>
+    </el-auto-resizer>
 </template>
 
-<script setup lang="ts">
+<script setup lang="tsx">
 import { onBeforeUnmount, onBeforeMount, ref, reactive } from 'vue';
 import { Editor, EditorContent } from '@tiptap/vue-3';
+import type { Column } from 'element-plus';
+import {
+    ElButton,
+    ElIcon,
+    ElTag,
+    ElSpace,
+    ElTooltip,
+    TableV2FixedDir,
+} from 'element-plus';
+import { Filter } from '@element-plus/icons-vue';
+import type { HeaderCellSlotProps } from 'element-plus';
 import { useRoute } from 'vue-router';
 import store from '../store';
 const colors = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928', '#FFD70', '#C0C0C0'];
@@ -37,6 +52,13 @@ const arr = ref<Array<ICONLL>>();
 const html = ref('');
 const id = String(vuerouter.params.id);
 const tags = ref();
+
+const columns: Column<any>[] = [
+    { key: 'form', title: store.loc('word'), width: 100, dataKey: 'form' },
+    { key: 'lemma', title: store.loc('lemma'), width: 100, dataKey: 'lemma' },
+    { key: 'upos', dataKey: 'upos', width: 150, title: 'PoS', cellRenderer: ({ cellData: upos }) => <ElTag disable-transitions color={colors[tags.value.indexOf(upos)]}>{upos} </ElTag>, },
+    { key: 'feats', dataKey: 'feats', width: 400, title: 'Features', cellRenderer: ({ cellData: feats }) => feats ? <ElSpace> {Object.entries(feats)?.map((x: any) => <ElTooltip content={x[0]}><ElTag disable-transitions> {x?.[1]} </ElTag></ElTooltip>)} </ElSpace> : '', },
+];
 
 const filterTag = (value: string, row: ICONLL) => {
     return row.upos === value
@@ -51,6 +73,10 @@ onBeforeMount(async () => {
             // console.log('this work', arr.value);
             tags.value = [...new Set(data.map((x: any) => x.upos))]
         }
+    }
+    if (id) {
+        const { data } = await store.getData('works', id);
+        work.value = data.shift()
     }
 });
 </script>

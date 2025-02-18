d4e999bb08ca700fe8393a3d0453ce422a7b7ba9
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-02-18 23:52:13 +0100

    import Day.js locales [front]

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import ElementPlus from 'element-plus';
-// import 'element-plus/lib/theme-chalk/index.css'
 import 'element-plus/dist/index.css';
-import locale from 'element-plus/dist/locale/ru.min.mjs';
+import 'dayjs/locale/en';
 import 'dayjs/locale/ru';
+import 'dayjs/locale/be';
 import router from './router';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import { pageTitle } from 'vue-page-title';
@@ -14,4 +14,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(`el-icon-${key.toLowerCase()}`, component);
 }
 
-app.use(pageTitle()).use(router).use(ElementPlus, { locale }).mount('#app');
+app.use(pageTitle()).use(router).use(ElementPlus).mount('#app');
